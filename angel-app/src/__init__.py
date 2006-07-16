f43247c45124a3f4421b75a01ef8aff49925b4a3
vincentk <vincentk@82c71e93-f314-0410-b1e0-bdb1255beee9>
2006-07-16 01:56:05 +0000

    documentation update (see the __init__.py files)
    git-svn-id: http://svn.gna.org/svn/angel-app/trunk@11 82c71e93-f314-0410-b1e0-bdb1255beee9

--- angel-app/src/__init__.py
+++ angel-app/src/__init__.py
@@ -1,1 +1,7 @@
 all = ["bin", "angel_app"]
+"""
+
+bin: binaries to be executed
+angel_app: all the interesting library stuff
+
+"""
--- angel-app/src/angel_app/__init__.py
+++ angel-app/src/angel_app/__init__.py
@@ -1,1 +1,10 @@
 all = ["crypto", "elements", "fileop", "static", "method"]
+
+"""
+crypto:   trivial ezPyCrypto stuff
+elements: angel-app specific WebDAV XML element definitions
+fileop:   we need to override some operations from twisted.web2.dav.fileop.py
+static:   the main angel-app data-structure ('AngelFile'). see also twisted.web2.dav.static.py and twisted.web.static.py
+method:   hacked versions of the http_* methods (exposed as http methods, e.g. GET, PUT etc.); see also twisted.web2.dav.method
+
+"""
