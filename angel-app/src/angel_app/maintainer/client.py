42c7e91f9a002d5b2d53c7aec6d26c07adda4e17
vincentk <vincentk@82c71e93-f314-0410-b1e0-bdb1255beee9>
2006-07-31 20:57:17 +0000

    parsing of response to validation request
    git-svn-id: http://svn.gna.org/svn/angel-app/trunk@23 82c71e93-f314-0410-b1e0-bdb1255beee9

--- angel-app/src/angel_app/maintainer/client.py
+++ angel-app/src/angel_app/maintainer/client.py
@@ -1,7 +1,8 @@
 from twisted.python import log
 from twisted.web2 import responsecode
 from twisted.web2.dav.element.rfc2518 import PropertyFind, PropertyContainer
-from twisted.python.filepath import FilePath
+from twisted.web2.dav.element import base
+from twisted.web2.dav import davxml
 
 from config.common import rootDir
 from angel_app.static import AngelFile
@@ -22,32 +23,85 @@ def testConn(host = "localhost", port = 9999):
     body = resp.read()
     conn.close()
 
-def validateClone(url):
+def validationKeys():
+    return elements.signedKeys + [elements.MetaDataSignature]
+
+def validationRequestXML():
+    """
+    the (xml) body of the validation request.
+    """
+    return PropertyFind(
+                        PropertyContainer(
+                                          *[key() for key in validationKeys()]
+                                          )).toxml()
+
+def validateClone(url, af):
     """
     @param url -- url of the clone to be validated
+    @param af -- angel file to be validated against
     
     exeptions wherever they might occur are passed on to the next level (i.e. not handled),
     signalling that this (potential) clone should be ignored from now on.
     """
     DEBUG and log.err(url)
     conn = HTTPConnection(url)
-
-    requestBody = PropertyFind(
-                               PropertyContainer(
-                                                 *[
-                                                   elements.Revision(),
-                                                   elements.PublicKeyString()
-                                                   ]
-                                                 ))
     
-    DEBUG and log.err(requestBody.toxml())
+    conn.request(
+                 "PROPFIND", 
+                 "/", 
+                 headers = {"Depth" : 0}, 
+                 body = validationRequestXML()
+                 )
     
-    conn.request("PROPFIND", "/", headers = {"Depth" : 0}, body = requestBody.toxml())
     resp = conn.getresponse()
     if resp.status != responsecode.MULTI_STATUS:
         raise "must receive a MULTI_STATUS response for PROPFIND, otherwise something's wrong"
-    body = resp.read()
-    print body
+    
+    responseBody = resp.read()
+    #print responseBody
+    
+    
+    msr = davxml.WebDAVDocument.fromString(responseBody)
+    
+    # points to the dav "prop"-element
+    properties = msr.root_element.children[0].children[1].children[0]
+    
+    #print `properties.children`
+    
+    # parse the revision number
+    #if not isinstance(properties.children[0], elements.Revision):# or \
+#    not isinstance(properties.children[0].children[0], base.PCDATAElement):
+    #    raise "illegal element for revision: " + `properties.children[0]` + `properties.children[0].__class__`
+    revision = str(properties.children[0].children[0])
+    #print `revision`
+
+    contentSignatureElements = properties.children[1]
+    #print publicKeyElements
+    #if not isinstance(properties.children[1], elements.ContentSignature) or \
+    #not [ee for ee in properties.children[1].children if isinstance(ee, base.PCDATAElement)]: 
+    #    raise "illegal element(s) for meta data signature"
+    contentSignature = "".join([str(ee) for ee in contentSignatureElements.children])
+    #print contentSignature
+  
+    publicKeyElements = properties.children[2]
+    #print publicKeyElements
+    #if not isinstance(properties.children[1], elements.ContentSignature) or \
+    #not [ee for ee in properties.children[1].children if isinstance(ee, base.PCDATAElement)]: 
+    #    raise "illegal element(s) for meta data signature"
+    publicKeyString = "".join([str(ee) for ee in publicKeyElements.children])
+    #print publicKeyString
+    
+    isDeleted = str(properties.children[3].children[0])
+    #print `isDeleted`
+    
+    metaDataSignatureElements = properties.children[4]
+    #print metaDataSignatureElements
+    #if not isinstance(properties.children[1], elements.ContentSignature) or \
+    #not [ee for ee in properties.children[1].children if isinstance(ee, base.PCDATAElement)]: 
+    #    raise "illegal element(s) for meta data signature"
+    metaDataSignature = "".join([str(ee) for ee in metaDataSignatureElements.children])
+    #print metaDataSignature 
+    
     conn.close()
 
 def updateClones(af):
@@ -65,7 +119,7 @@ def updateClones(af):
     
     for clone in clones.children:
        url = str(clone.children[0].children[0])
-       validateClone(url)
+       validateClone(url, af)
 
 def inspectResource(path = rootDir):
     #if DEBUG and relativePath(resource.path) != "": raise "debugging and stopping beyond root: " + relativePath(resource.path)
