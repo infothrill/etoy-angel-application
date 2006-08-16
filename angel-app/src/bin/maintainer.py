16c2b3da3bf61ce55d9ff82cea6082570dfad3a5
vincentk <vincentk@82c71e93-f314-0410-b1e0-bdb1255beee9>
2006-08-16 19:20:17 +0000

    better variable naming
    git-svn-id: http://svn.gna.org/svn/angel-app/trunk@37 82c71e93-f314-0410-b1e0-bdb1255beee9

--- angel-app/src/bin/maintainer.py
+++ angel-app/src/bin/maintainer.py
@@ -7,9 +7,9 @@ from config.common import rootDir
 
 from os import walk
 import os.path 
-for item in walk(rootDir): 
-    print "walking: " + item[0]
-    inspectResource(item[0])
-    for fileItem in item[2]:
-        inspectResource(os.path.join(item[0], fileItem))
+for directory, subDirectories, fileNames in walk(rootDir): 
+    print "walking: " + directory
+    inspectResource(directory)
+    for name in fileNames:
+        inspectResource(os.path.join(directory, name))
 
