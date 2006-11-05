62dde67b8080342d6222fd5369db7825a2461583
vincentk <vincentk@82c71e93-f314-0410-b1e0-bdb1255beee9>
2006-11-05 14:25:07 +0000

    first version of proppatch working
    git-svn-id: http://svn.gna.org/svn/angel-app/trunk@85 82c71e93-f314-0410-b1e0-bdb1255beee9

--- angel-app/src/angel_app/davMethods/proppatch.py
+++ angel-app/src/angel_app/davMethods/proppatch.py
@@ -35,8 +35,118 @@ from twisted.internet.defer import deferredGenerator
 class ProppatchMixin:
     
     def __proppatchPreconditions(self, request):
+        
+        if not self.fp.exists():
+            log.err("File not found: %s" % (self.fp.path,))
+            raise HTTPError(responsecode.NOT_FOUND)
+        
         log.err("proppatch preconditions")
         yield request
     
     def preconditions_PROPPATCH(self, request):
         return deferredGenerator(self.__proppatchPreconditions)(request)
+
+    def authenticate(self, requestProperties):
+        pass
+
+    def apply(self, requestProperties, uri):
+
+        responses = PropertyStatusResponseQueue(
+                                    "PROPPATCH", 
+                                    request.uri, 
+                                    responsecode.NO_CONTENT)
+        
+        dp = self.deadProperties()
+        
+        for prop in requestProperties:
+            try:
+                dp.set(prop)
+            except ValueError, err:
+                responses.add(
+                        Failure(
+                            exc_value=HTTPError(
+                                StatusResponse(
+                                   responsecode.FORBIDDEN, str(e)))),
+                        property
+                    )
+            except:
+                responses.add(Failure(), property)
+            else:
+                responses.add(responsecode.OK, property)
+            
+        return responses
+
+
+    def __proppatch(self, request):
+        
+        # read the body
+        doc = waitForDeferred(deferredGenerator(readRequestBody)(request))
+        yield doc
+        doc = doc.geResult()
+        
+        # perform basic validation
+        validateBodyXML(doc)
+        
+        # extract the properties to be patched
+        requestProperties = getRequestProperties(doc)
+        
+        # authenticate
+        self.authenticate(requestProperties)
+        
+        # apply the changes
+        yield self.apply(requestProperties)
+
+        
+    def http_PROPPATCH(self, request):
+        return deferredGenerator(self.__proppatch)(request)
+
+
+def readRequestBody(request):
+    """
+    Read XML body from request stream.
+    """
+    try:
+        doc = waitForDeferred(davXMLFromStream(request.stream))
+        yield doc
+        doc = doc.getResult()
+    except ValueError, e:
+        log.err("Error while handling PROPPATCH body: %s" % (e,))
+        raise HTTPError(StatusResponse(responsecode.BAD_REQUEST, str(e)))
+
+    if doc is None:
+        error = "Request XML body is required."
+        log.err(error)
+        raise HTTPError(StatusResponse(responsecode.BAD_REQUEST, error))
+
+    yield doc
+
+def getRequestProperties(doc):
+    return [
+     ee.children[0]
+     for ee in doc.root_element.children
+    ]
+
+def validateBodyXML(doc):
+    """
+    Parse request
+    """
+    update = doc.root_element
+    if not isinstance(update, davxml.PropertyUpdate):
+        error = ("Request XML body must be a propertyupdate element."
+                 % (davxml.PropertyUpdate.sname(),))
+        log.err(error)
+        raise HTTPError(StatusResponse(responsecode.BAD_REQUEST, error))
+    
+    for child in update.children:
+        
+        if not isinstance(child, davxml.Set):
+            error = "We don't currently allow property removal via proppatch. Only SET tags are allowed."
+            raise HTTPError(StatusResponse(responsecode.BAD_REQUEST, error))
+        
+        if not len(child.children == 1):
+            error = "All SET tags must contain exactly one PropertyContainer tag."
+            raise HTTPError(StatusResponse(responsecode.BAD_REQUEST, error))
+        
+        if not isinstance(child.children[0], davxml.ProperyContainer):
+            error = "All SET tags must contain exactly one PropertyContainer tag."
+            raise HTTPError(StatusResponse(responsecode.BAD_REQUEST, error))
