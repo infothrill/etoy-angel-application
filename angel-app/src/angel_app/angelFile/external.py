d7be3733beba25db70fbe2fc8d40ff2da236c6a9
vincentk <vincentk@82c71e93-f314-0410-b1e0-bdb1255beee9>
2006-11-23 14:08:45 +0000

    moved PROPPATCH from Basic to External, where it belongs
    git-svn-id: http://svn.gna.org/svn/angel-app/trunk@112 82c71e93-f314-0410-b1e0-bdb1255beee9

--- angel-app/src/angel_app/angelFile/external.py
+++ angel-app/src/angel_app/angelFile/external.py
@@ -6,6 +6,7 @@ from twisted.web2 import http, stream
 from twisted.web2.dav.xattrprops import xattrPropertyStore
 from angel_app import elements
 from angel_app.angelFile.basic import Basic
+from angel_app.davMethods.proppatch import ProppatchMixin
 
 DEBUG = False
 
