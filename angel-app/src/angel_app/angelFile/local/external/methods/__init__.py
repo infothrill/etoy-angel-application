e65323b36e9921f3f8e3573b2fd2e428e66a0d91
vincentk <vincentk@82c71e93-f314-0410-b1e0-bdb1255beee9>
2006-12-04 11:14:41 +0000

    git-svn-id: http://svn.gna.org/svn/angel-app/trunk@164 82c71e93-f314-0410-b1e0-bdb1255beee9

--- angel-app/src/angel_app/angelFile/local/external/methods/__init__.py
+++
@@ -1,6 +0,0 @@
-all = ["mkcol","proppatch", "put"]
-
-"""
-Mixins for AngelFile behaviour. Each mixin implements one WebDAV method (PUT, LOCK, etc.).
-I feel this is cleaner than wilfredo's bindmethods() - approach.
-"""
--- angel-app/src/angel_app/angelFile/local/internal/__init__.py
+++
@@ -1,1 +0,0 @@
-all = ["methods", "resource"]
