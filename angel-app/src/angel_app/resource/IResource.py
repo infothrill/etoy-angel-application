258998656fbb78a4f32ae0a13d0a003545240e06
vincentk <vincentk@82c71e93-f314-0410-b1e0-bdb1255beee9>
2006-12-05 07:53:55 +0000

    adding angelResource interface
    git-svn-id: http://svn.gna.org/svn/angel-app/trunk@170 82c71e93-f314-0410-b1e0-bdb1255beee9

--- angel-app/src/angel_app/resource/IResource.py
+++ angel-app/src/angel_app/resource/IResource.py
@@ -1,3 +1,35 @@
+"""
+Interface to be provided by all angel-app resources.
+"""
+
+legalMatters = """
+ Copyright (c) 2006, etoy.VENTURE ASSOCIATION
+ All rights reserved.
+ 
+ Redistribution and use in source and binary forms, with or without modification, 
+ are permitted provided that the following conditions are met:
+ *  Redistributions of source code must retain the above copyright notice, 
+    this list of conditions and the following disclaimer.
+ *  Redistributions in binary form must reproduce the above copyright notice, 
+    this list of conditions and the following disclaimer in the documentation 
+    and/or other materials provided with the distribution.
+ *  Neither the name of etoy.CORPORATION nor the names of its contributors may be used to 
+    endorse or promote products derived from this software without specific prior 
+    written permission.
+ 
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY 
+ EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES 
+ OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT 
+ SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+ SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT 
+ OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 
+ HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, 
+ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
+ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
+"""
+
+author = """Vincent Kraeutler, 2006"""
+
 
 import zope.interface
 
@@ -8,71 +40,42 @@ class AngelResource(zope.interface.Interface):
     
     def exists(self):
         """
-        Checks whether this resource is accesible.
+        @return: a C{True} if this resource is accessible, C{False} otherwise.
         """
     
     def isCollection():
         """
-        Checks whether this resource is a collection resource.
-        @return: C{True} if this resource is a collection resource, C{False}
+        Checks whether this resource is a collection resource / directory.
+        @return: a C{True} if this resource is a collection resource, C{False}
             otherwise.
         """
-
-    def findChildren(depth):
+        
+    def revision():
         """
-        Returns an iterable of child resources for the given depth.
-        Because resources do not know their request URIs, chidren are returned
-        as tuples C{(resource, uri)}, where C{resource} is the child resource
-        and C{uri} is a URL path relative to this resource.
-        @param depth: the search depth (one of C{"0"}, C{"1"}, or C{"infinity"})
-        @return: an iterable of tuples C{(resource, uri)}.
+        @return: a C{int} corresponding to the revision number of this resource
         """
 
-    def hasProperty(property, request):
+    def findChildren():
         """
-        Checks whether the given property is defined on this resource.
-        @param property: an empty L{davxml.WebDAVElement} instance or a qname
-            tuple.
-        @param request: the request being processed.
-        @return: a deferred value of C{True} if the given property is set on
-            this resource, or C{False} otherwise.
+        @return: an iterable over C{uri}.
         """
 
-    def readProperty(property, request):
+    def getProperty(property):
         """
         Reads the given property on this resource.
         @param property: an empty L{davxml.WebDAVElement} class or instance, or
             a qname tuple.
-        @param request: the request being processed.
-        @return: a deferred L{davxml.WebDAVElement} instance
+        @return: a L{davxml.WebDAVElement} instance
             containing the value of the given property.
-        @raise HTTPError: (containing a response with a status code of
-            L{responsecode.CONFLICT}) if C{property} is not set on this
-            resource.
         """
 
-    def writeProperty(property, request):
+    def writeProperties(properties):
         """
         Writes the given property on this resource.
-        @param property: a L{davxml.WebDAVElement} instance.
-        @param request: the request being processed.
-        @return: an empty deferred which fires when the operation is completed.
-        @raise HTTPError: (containing a response with a status code of
-            L{responsecode.CONFLICT}) if C{property} is a read-only property.
+        @param properties a the list of elements.requiredKeys
         """
 
-    def removeProperty(property, request):
-        """
-        Removes the given property from this resource.
-        @param property: a L{davxml.WebDAVElement} instance or a qname tuple.
-        @param request: the request being processed.
-        @return: an empty deferred which fires when the operation is completed.
-        @raise HTTPError: (containing a response with a status code of
-            L{responsecode.CONFLICT}) if C{property} is a read-only property or
-            if the property does not exist.
-        """
-
-    def listProperties(request):
+    def listProperties():
         """
         @param request: the request being processed.
         @return: a deferred iterable of qnames for all properties defined for
