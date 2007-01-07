25174727329177b65386a9b620aaf4e35ed72dd6
pkremer <pkremer@82c71e93-f314-0410-b1e0-bdb1255beee9>
2007-01-07 18:15:03 +0000

    coding style (Uppercase Class names), fix logging protocol to loop over he buffer until it is exhausted.
    git-svn-id: http://svn.gna.org/svn/angel-app/trunk@243 82c71e93-f314-0410-b1e0-bdb1255beee9

--- angel-app/src/bin/master.py
+++ angel-app/src/bin/master.py
@@ -36,13 +36,13 @@ class ExternalProcessProtocol(ProcessProtocol):
 		getLogger().warn("external Process with protocol '%s' ended with reason: '%s'" , self.__class__.__name__, reason.getErrorMessage())
 		endedProc(self.__class__.__name__)
 
-class presenterProtocol(ExternalProcessProtocol):
+class PresenterProtocol(ExternalProcessProtocol):
 	def dummy():
 		pass
-class providerProtocol(ExternalProcessProtocol):
+class ProviderProtocol(ExternalProcessProtocol):
 	def dummy():
 		pass
-class maintainerProtocol(ExternalProcessProtocol):
+class MaintainerProtocol(ExternalProcessProtocol):
 	def dummy():
 		pass
 
@@ -57,17 +57,17 @@ def endedProc(name):
 def startProc(name):
 	import re
 	executable = "python"
-	if re.match('^presenter.*', name):
-		proto = presenterProtocol()
-		args = ["python", os.path.join(os.getcwd(),"bin/presenter.py"), '-l']
+	if re.match('^Presenter.*', name):
+		proto = PresenterProtocol()
+		args = [executable, os.path.join(os.getcwd(),"bin/presenter.py"), '-l']
 		presenterTransport = reactor.spawnProcess(proto, executable, args, env=os.environ, path='/', uid=None, gid=None, usePTY=True)
-	if re.match('^provider.*', name):
-		proto = providerProtocol()
-		args = ["python", os.path.join(os.getcwd(),"bin/provider.py"), '-l']
+	if re.match('^Provider.*', name):
+		proto = ProviderProtocol()
+		args = [executable, os.path.join(os.getcwd(),"bin/provider.py"), '-l']
 		providerTransport = reactor.spawnProcess(proto, executable, args, env=os.environ, path='/', uid=None, gid=None, usePTY=True)
-	if re.match('^maintainer.*', name):
-		proto = maintainerProtocol()
-		args = ["python", os.path.join(os.getcwd(),"bin/maintainer.py"), '-l']
+	if re.match('^Maintainer.*', name):
+		proto = MaintainerProtocol()
+		args = [executable, os.path.join(os.getcwd(),"bin/maintainer.py"), '-l']
 		providerTransport = reactor.spawnProcess(proto, executable, args, env=os.environ, path='/', uid=None, gid=None, usePTY=True)
 	# give the proc a chance to start:
 	import time
@@ -78,7 +78,7 @@ import struct
 import cPickle
 class LoggingProtocol(Protocol):
 	"""
-	Simplistic logging protocol as given by the "standard" python logging module and
+	Logging protocol as given by the "standard" python logging module and
 	its SocketHandler: http://docs.python.org/lib/network-logging.html
 	"""
 	def connectionMade(self):
@@ -100,7 +100,7 @@ class LoggingProtocol(Protocol):
 	def dataReceived(self, data):
 		self.buf += data
 		# first 4 bytes specify the length of the pickle
-		if len(self.buf) >= 4:
+		while len(self.buf) >= 4:
 			if self.slen == 0:
 				#print "buf longer than 4, finding slen"
 				self.slen = struct.unpack(">L", self.buf[0:4])[0]
@@ -112,10 +112,10 @@ class LoggingProtocol(Protocol):
 				except:
 					getLogger().error("Problem unpickling")
 				else:
-					self.buf = self.buf[self.slen+4:]
-					self.slen = 0
 					record = logging.makeLogRecord(obj)
 					self.handleLogRecord(record)
+				self.buf = self.buf[self.slen+4:]
+				self.slen = 0
 
 	def handleLogRecord(self, record):
 		logger = logging.getLogger(record.name)
@@ -140,8 +140,8 @@ if __name__ == "__main__":
 	from logging.handlers import DEFAULT_TCP_LOGGING_PORT
 	reactor.listenTCP(DEFAULT_TCP_LOGGING_PORT, factory)
 
-	startProc('presenter')
-	startProc('provider')
-	#startProc('maintainer')
+	startProc('Presenter')
+	startProc('Provider')
+	#startProc('Maintainer')
 
 	reactor.run()
