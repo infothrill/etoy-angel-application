e663c6e9525feab453c9a85c0423d0b2d624a01c
vincentk <vincentk@82c71e93-f314-0410-b1e0-bdb1255beee9>
2007-01-13 18:45:39 +0000

    move from twisted logger to angel_app logger
    git-svn-id: http://svn.gna.org/svn/angel-app/trunk@263 82c71e93-f314-0410-b1e0-bdb1255beee9

--- angel-app/src/angel_app/resource/local/internal/resource.py
+++ angel-app/src/angel_app/resource/local/internal/resource.py
@@ -1,5 +1,6 @@
 import os
-from twisted.python import log
+#from twisted.python import log
+from angel_app.log import getLogger
 from twisted.web2 import stream
 from twisted.web2.dav.element import rfc2518
 from angel_app import elements
@@ -10,6 +11,7 @@ from angel_app.config import internal as config
 
 DEBUG = True
 
+log = getLogger()
 # DO NOT EXPOSE THIS KEY!!!!
 from angel_app.config.internal import loadKeysFromFile
 
@@ -42,6 +44,7 @@ class Crypto(
         """
         Set all required properties to a syntactically meaningful default value, if not already set.
         """
+        DEBUG and log.debug("_initProperties for " + self.fp.path)
         dp = self.deadProperties()
         for element in elements.requiredKeys:
             if not dp.contains(element.qname()):
@@ -50,8 +53,9 @@ class Crypto(
                 else:  
                     ee = element()  
                 
-                DEBUG and log.err("initializing " + element.sname() + " of " + self.fp.path + " to " + ee.toxml())
+                DEBUG and log.debug("initializing " + element.sname() + " of " + self.fp.path + " to " + ee.toxml())
                 dp.set(ee)
+        DEBUG and log.debug("done _initProperties for: " + self.fp.path)
                     
         
     def _inheritClones(self):
@@ -85,12 +89,12 @@ class Crypto(
         plainText = myFile.read()
         myFile.close()
         cypherText = self.secretKey.encString(plainText)
-        DEBUG and log.err("encrypting file: " + self.fp.path)
+        DEBUG and log.error("encrypting file: " + self.fp.path)
         myFile = self.fp.open("w") 
         myFile.write(cypherText)
         myFile.flush()
         myFile.close()
-        DEBUG and log.err(cypherText)  
+        DEBUG and log.error(cypherText)  
 
     def sign(self):
         """
@@ -98,7 +102,7 @@ class Crypto(
         If the secretKey is 
         """
         
-        DEBUG and log.err("signing file: " + self.fp.path)
+        DEBUG and log.error("signing file: " + self.fp.path)
         
         # TODO: this sucks, too
         if self.fp.isdir(): 
@@ -123,7 +127,7 @@ class Crypto(
         Increase the revision number by one, if it not initialized, set it to 1.
         """
         nn = self.revisionNumber() + 1
-        DEBUG and log.err("revision number for " + self.fp.path +" now at: " + `nn`)
+        DEBUG and log.error("revision number for " + self.fp.path +" now at: " + `nn`)
         self.deadProperties().set(elements.Revision.fromString(`nn`))
         return int(nn)
 
@@ -138,11 +142,11 @@ class Crypto(
         @returns True if the location is writable, False otherwise
         """
         
-        DEBUG and log.err("testing for writability of: " + self.fp.path)
+        DEBUG and log.error("testing for writability of: " + self.fp.path)
         
         if not self.secretKey:
             # we don't even have a private key
-            DEBUG and log.err("Crypto: no key available")
+            DEBUG and log.error("Crypto: no key available")
             return False
 
         
@@ -154,7 +158,7 @@ class Crypto(
                 return False
             
             if not self.parent().exists():
-                DEBUG and log.err("this is not the root, but the parent directory does not exist")
+                DEBUG and log.error("this is not the root, but the parent directory does not exist")
                 return False
             else:
                 return self.parent().isWritableFile()
@@ -162,7 +166,7 @@ class Crypto(
                 
         myKeyString = self.secretKey.exportKey()    
         fileKeyString = self.getOrSet(elements.PublicKeyString, myKeyString)
-        DEBUG and log.err("public key for " + self.fp.path + ": " + fileKeyString)
+        DEBUG and log.error("public key for " + self.fp.path + ": " + fileKeyString)
         return fileKeyString == myKeyString
 
 
@@ -176,70 +180,89 @@ class Crypto(
         See also: L{ezPyCrypto.key}
         """
         
-        DEBUG and log.err("Crypto: sealing " + self.fp.path)
-        DEBUG and log.err("Crypto: signable data: " + self.signableMetadata())
+        DEBUG and log.error("Crypto: sealing " + self.fp.path)
+        DEBUG and log.error("Crypto: signable data: " + self.signableMetadata())
         signature = self.secretKey.signString(self.signableMetadata())
         self.deadProperties().set(elements.MetaDataSignature.fromString(signature))
         #storedsignature = self.getOrSet(elements.MetaDataSignature, "0")
-        #log.err(signature)
-        #log.err(storedsignature)
-        DEBUG and log.err("Crypto: signature is " + signature)
+        #log.error(signature)
+        #log.error(storedsignature)
+        DEBUG and log.error("Crypto: signature is " + signature)
         return signature
     
     def updateParent(self, recursionLimit = 0):
         pp = self.parent()
-        log.err(pp.fp.path)
+        log.error(pp.fp.path)
         pp and pp.update()
 
     def _deRegisterWithParent(self):
 
-        DEBUG and log.err("entering _deRegisterWithParent")
+        DEBUG and log.error("entering _deRegisterWithParent for: " + self.fp.path)
         
+        DEBUG and log.error(`self.parent()`)
         pdp = self.parent().deadProperties()
         
         oc = pdp.get(elements.Children.qname()).children
-        DEBUG and log.err(len(oc))
+        DEBUG and log.error(len(oc))
         
-        DEBUG and log.err("resourceName: " + self.resourceName())     
+        DEBUG and log.error("resourceName: " + self.resourceName())     
         nc = [cc for cc in oc if not str(cc.childOfType(rfc2518.HRef)) == self.resourceName()]
         
         pdp.set(elements.Children(*nc))
-        DEBUG and log.err("exiting _deRegisterWithParent")
+        DEBUG and log.error("exiting _deRegisterWithParent")
     
     def _registerWithParent(self):
 
-        DEBUG and log.err("entering _registerWithParent for " + self.fp.path)
+        DEBUG and log.error("entering _registerWithParent for " + self.fp.path)
 
-        DEBUG and log.err("make sure the required properties are initialized")
+        DEBUG and log.error("make sure the required properties are initialized")
         self._initProperties()
         
         pdp = self.parent().deadProperties()
         
         oc = pdp.get(elements.Children.qname()).children
-        DEBUG and log.err(len(oc))
+        DEBUG and log.error(len(oc))
         
-        DEBUG and log.err("resourceName: " + self.resourceName())  
-        log.err("foo")   
-        DEBUG and log.err("resourceID: " + `self.resourceID()`)    
+        DEBUG and log.error("resourceName: " + self.resourceName())  
+        log.error("foo")   
+        DEBUG and log.error("resourceID: " + `self.resourceID()`)    
         for cc in oc:
-            DEBUG and log.err("child: " + str(cc.childOfType(rfc2518.HRef)))
+            DEBUG and log.error("child: " + str(cc.childOfType(rfc2518.HRef)))
             if str(cc.childOfType(rfc2518.HRef)) == self.resourceName():
-                DEBUG and log.err(self.fp.path + ": this resource is already registered with the parent")
+                DEBUG and log.error(self.fp.path + ": this resource is already registered with the parent")
                 return
         
-        DEBUG and log.err("foo")
+        DEBUG and log.error("foo")
         ic = elements.Child(*[
                          rfc2518.HRef(self.resourceName()),
                          elements.PublicKeyString(self.parent().publicKeyString()),
                          self.resourceID()
                          ])
-        DEBUG and log.err("bar")
+        DEBUG and log.error("bar")
 
         
         nc = [cc for cc in oc] + [ic]
         
-        pdp.set(elements.Children(*nc))
-        DEBUG and log.err("exiting _registerWithParent")
+        log.error(nc)
+        ce = elements.Children(*nc)
+        log.error(ce.toxml())
+        log.error(`pdp`)
+        log.error("foobar")
+        
+        try:
+            log.error(self.parent().exists())
+            pdp.set(ce)
+            log.error("foobar2")
+        except:
+            import sys, traceback
+            msg = apply(traceback.format_exception,sys.exc_info())
+            log.error(msg)
+            errorMessage = "Unable to update child elements."
+            log.error(errorMessage)
+            raise
+            
+            
+        log.error("exiting _registerWithParent")
     
         
         
@@ -254,7 +277,7 @@ class Crypto(
             self._deRegisterWithParent()
             
             destination_uri = request.headers.getHeader("destination")
-            DEBUG and log.err("changeRegister: " + `self.__class__`)
+            DEBUG and log.error("changeRegister: " + `self.__class__`)
             destination = resourceFromURI(destination_uri, self.__class__)
             destination._registerWithParent()
         
@@ -265,31 +288,31 @@ class Crypto(
     
     def update(self, recursionLimit = 0):
         
-        DEBUG and log.err("called update on: " + self.fp.path)
+        DEBUG and log.error("called update on: " + self.fp.path)
         
         if not self.isWritableFile(): 
             raise "not authorized to perform update of signed meta data"
 
-        DEBUG and log.err("encrypting " + self.fp.path + "?")
+        DEBUG and log.error("encrypting " + self.fp.path + "?")
         if self.isEncrypted():
-            DEBUG and log.err("encrypting " + self.fp.path)
+            DEBUG and log.error("encrypting " + self.fp.path)
             self.encrypt()
-        DEBUG and log.err("signing " + self.fp.path)
+        DEBUG and log.error("signing " + self.fp.path)
         self.sign()
-        DEBUG and log.err("bumping revision number for " + self.fp.path)
+        DEBUG and log.error("bumping revision number for " + self.fp.path)
         self.bumpRevisionNumber()
 
         
-        DEBUG and log.err("sealing " + self.fp.path)
+        DEBUG and log.error("sealing " + self.fp.path)
         self.seal()
 
-        DEBUG and log.err("Verifying " + self.fp.path)
+        DEBUG and log.error("Verifying " + self.fp.path)
         DEBUG and self.verify()
         
         # certainly not going to hurt if we do this:
         self.fp.restat()
         
-        log.err(self.fp.path + " now at revision: " + self.getOrSet(elements.Revision)) 
+        log.error(self.fp.path + " now at revision: " + self.getOrSet(elements.Revision)) 
         if recursionLimit > 0:
             self.updateParent(recursionLimit - 1)
     
@@ -298,7 +321,7 @@ class Crypto(
         """
         @see Basic.getResponseStream
         """
-        DEBUG and log.err("rendering file in plaintext: " + self.fp.path)
+        DEBUG and log.error("rendering file in plaintext: " + self.fp.path)
         if self.isEncrypted():
             
             fileContents = self.secretKey.decString(self.fp.open().read())
