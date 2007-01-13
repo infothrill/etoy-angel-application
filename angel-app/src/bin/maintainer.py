1eb64887cd99d5a32848de40af56029d72b7a951
pkremer <pkremer@82c71e93-f314-0410-b1e0-bdb1255beee9>
2007-01-13 14:21:45 +0000

    use our logging, enable daemon mode
    git-svn-id: http://svn.gna.org/svn/angel-app/trunk@257 82c71e93-f314-0410-b1e0-bdb1255beee9

--- angel-app/src/bin/maintainer.py
+++ angel-app/src/bin/maintainer.py
@@ -1,3 +1,6 @@
+from optparse import OptionParser
+from angel_app.log import getLogger
+
 from angel_app.resource.remote import client, setup
 from angel_app.graph import graphWalker
 from twisted.python import log
@@ -7,19 +10,25 @@ setup.setupDefaultPeers()
 
 from angel_app.config import config
 AngelConfig = config.getConfig()
-repository = AngelConfig.get("common","repository")
+repository = AngelConfig.get("common", "repository")
 
-    
-# TODO: ugly twisted workaround to provide angel_app xml elements
-from twisted.web2.dav.element import parser
-from angel_app import elements
-parser.registerElements(elements)
+def bootInit():
+	"""
+	Method to be called in __main__ before anything else. This method cannot rely on any
+	framework being initialised, e.g. no logging, no exception catching etc.
+	"""
+	import angel_app.config.defaults
+	angel_app.config.defaults.appname = "maintainer"
+	
+	
+	# TODO: ugly twisted workaround to provide angel_app xml elements
+	from twisted.web2.dav.element import parser
+	from angel_app import elements
+	parser.registerElements(elements)
 
-DEBUG = True
-            
-if __name__ == "__main__":
+def runServer():
     log.err("starting inspection loop at: " + repository)
-    
+
     def getChildren(path):
         children = [cc.fp.path for cc in Basic(path).metaDataChildren()]
         DEBUG and log.err("children of " + path + " are " + `children`)
@@ -36,3 +45,27 @@ if __name__ == "__main__":
     
     for ii in graphWalker(repository, getChildren, toEvaluate):
         continue
+            
+if __name__ == "__main__":
+    bootInit()
+    parser = OptionParser()
+    parser.add_option("-d", "--daemon", dest="daemon", help="daemon mode?", default='')
+    parser.add_option("-l", "--log", dest="networklogging", help="use network logging?", action="store_true" , default=False)
+    (options, args) = parser.parse_args()
+
+    import angel_app.log
+    angel_app.log.setup()
+    angel_app.log.enableHandler('file')
+    if len(options.daemon) > 0:
+        angel_app.log.enableHandler('socket')
+        from angel_app import proc
+        proc.startstop(action=options.daemon, stdout='maintainer.stdout', stderr='maintainer.stderr', pidfile='maintainer.pid')
+    else:
+        if (options.networklogging):
+            angel_app.log.enableHandler('socket')
+        else:
+            angel_app.log.enableHandler('console')
+    angel_app.log.getReady()
+    DEBUG = True
+    runServer()
+
