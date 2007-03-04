43737e9f20b0c187d272a309e59743ebe8640d03
pkremer <pkremer@82c71e93-f314-0410-b1e0-bdb1255beee9>
2007-03-04 11:35:59 +0000

    scripts to create graphical module dependency graphs of the source code. createModuleDepGraph.sh is the "frontend" script..
    git-svn-id: http://svn.gna.org/svn/angel-app/trunk@490 82c71e93-f314-0410-b1e0-bdb1255beee9

---
+++ angel-app/src/scripts/createModuleDepGraph.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ ! -f src/scripts/py2depgraph.py ]
+then
+	echo "You must execute this script from the base checkout directory!"
+	exit -1
+fi
+
+format=${1:?Please specify format (png or svg)}
+outdir=${2:?Please specify a directory for the output files!}
+
+if [ ! -d $outdir ]
+then
+	echo "Output dir $outdir does not exist or is not a directory"
+	exit -1
+fi
+
+for f in master presenter provider maintainer
+do
+	`PYTHONPATH=src/:$PYTHONPATH python src/scripts/py2depgraph.py src/bin/$f.py | python src/scripts/depgraph2dot.py | dot -T $format -o $outdir/$f.$format`
+done
+
