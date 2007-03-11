60af8ddf712d45be3be7f929ac131856ac93b916
pkremer <pkremer@82c71e93-f314-0410-b1e0-bdb1255beee9>
2007-03-11 17:18:51 +0000

    ongoing work
    git-svn-id: http://svn.gna.org/svn/angel-app/trunk@647 82c71e93-f314-0410-b1e0-bdb1255beee9

--- angel-app/src/angel_app/wx/main.py
+++ angel-app/src/angel_app/wx/main.py
@@ -1,5 +1,7 @@
 import wx
+import  wx.gizmos   as  gizmos
 
+import time
 import angel_app.wx.masterthread
 from angel_app.config import config
 AngelConfig = config.getConfig()
@@ -29,7 +31,6 @@ class AngelMainFrame(wx.Frame):
         self.help_menu = wx.Menu()
         self.help_menu.Append(wx.ID_ABOUT, "&About")
         self.Bind(wx.EVT_MENU, self.on_about_request, id=wx.ID_ABOUT)
-        self.Bind(wx.EVT_MENU, self.on_about_request, id=wx.ID_ABOUT)
         ID_HELP_WIKI = wx.NewId()
         self.help_menu.Append(ID_HELP_WIKI, "&Angel-App Wiki (Website)")
         self.Bind(wx.EVT_MENU, self.on_help_wiki, id=ID_HELP_WIKI)
@@ -37,15 +38,25 @@ class AngelMainFrame(wx.Frame):
         self.help_menu.Append(ID_HELP_M221E, "&MISSION ETERNITY (Website)")
         self.Bind(wx.EVT_MENU, self.on_help_m221e, id=ID_HELP_M221E)
 
+        ID_HELP_LICENSE = wx.NewId()
+        self.help_menu.Append(ID_HELP_LICENSE, "&License")
+        self.Bind(wx.EVT_MENU, self.on_help_license, id=ID_HELP_LICENSE)
+
         self.menu_bar.Append(self.help_menu, "&Help")
+
         self.SetMenuBar(self.menu_bar)
         # end define the menus
 
+        self.sb = AngelStatusBar(self)
+        self.SetStatusBar(self.sb)
+        
         self.SetBackgroundColour(wx.WHITE)
         self.bitmap = wx.Bitmap(M221E_LOGO_SMALL)
         wx.EVT_PAINT(self, self.OnPaint)
-        self.Centre()
         self.static_text = wx.StaticText(self, -1, "MISSION ETERNITY's Angel-App",style=wx.ALIGN_CENTRE)
+
+        self.Centre()
+
 
         _daemon = angel_app.wx.masterthread.MasterThread()
         _daemon.setDaemon(True)
@@ -53,6 +64,7 @@ class AngelMainFrame(wx.Frame):
         self.daemon = _daemon
 
         self.Bind(wx.EVT_CLOSE, self.OnQuit)
+
 
     def OnPaint(self, event):
         dc = wx.PaintDC(self)
@@ -63,10 +75,20 @@ class AngelMainFrame(wx.Frame):
         self.Destroy()
 
     def doExit(self, event):
-        print "closing"
+        print "Exiting on user request"
         self.Close(True)
-        
+
     def on_about_request(self, event):
+        # copyright symbol: \u00A9
+        dlg = wx.MessageDialog(self, u'Version pre-alpha0.1\n\u00A9 Copyright 2006-2007 etoy.VENTURE ASSOCIATION,\nall rights reserved', # TODO embed version string
+                               'Angel-App',
+                               wx.OK | wx.ICON_INFORMATION
+                               )
+        dlg.CenterOnParent()
+        dlg.ShowModal()
+        dlg.Destroy()
+        
+    def on_help_license(self, event):
         text = """Copyright (c) 2006-2007, etoy.VENTURE ASSOCIATION
 All rights reserved.
          
@@ -77,9 +99,11 @@ Redistribution and use in source and binary forms, with or without modification,
          
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
 """ 
-        from wxPython.lib.dialogs import wxScrolledMessageDialog # TODO:  DeprecationWarning: The wxPython compatibility package is no longer automatically generated or activly maintained.  Please switch to the wx package as soon as possible. from wxPython.lib.dialogs import wxScrolledMessageDialog
-        dlg = wxScrolledMessageDialog(self, text, 'About Angel-App')
+        from wx.lib.dialogs import ScrolledMessageDialog
+        dlg = ScrolledMessageDialog(self, text, 'Angel-App License Information')
+        dlg.CenterOnParent()
         dlg.ShowModal()
+        dlg.Destroy()
 
     def on_help_presenter(self, event):
         interface = AngelConfig.get("presenter", "listenInterface")
@@ -93,6 +117,27 @@ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
         wx.Execute("open http://www.missioneternity.org") # FIXME: only works on OS X
     
 
+class AngelStatusBar(wx.StatusBar):
+    def __init__(self, parent):
+        wx.StatusBar.__init__(self, parent, -1)
+
+        # This status bar has one field
+        self.SetFieldsCount(1)
+        # Sets the three fields to be relative widths to each other.
+        self.SetStatusWidths([-2])
+
+        # We're going to use a timer to drive a 'clock' in the last
+        # field.
+        self.timer = wx.PyTimer(self.Notify)
+        self.timer.Start(1000)
+        self.Notify()
+
+    # Handles events from the timer we started in __init__().
+    # We're using it to drive a 'clock' in field 0
+    def Notify(self):
+        t = time.localtime(time.time())
+        st = time.strftime("%d-%b-%Y   %H:%M:%S", t)
+        self.SetStatusText(st, 0)
 
 
 class AngelApp(wx.App):
