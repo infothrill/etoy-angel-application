033200863eae24e35713911ed35746eb678e727e
pkremer <pkremer@82c71e93-f314-0410-b1e0-bdb1255beee9>
2007-03-12 17:20:32 +0000

    updated readme, buildApp.sh now works unattended (with latest version of py2app)
    git-svn-id: http://svn.gna.org/svn/angel-app/trunk@710 82c71e93-f314-0410-b1e0-bdb1255beee9

--- angel-app/distrib/OSX/buildApp.sh
+++ angel-app/distrib/OSX/buildApp.sh
@@ -1,6 +1,30 @@
-#!/bin/sh
+#!/bin/bash
+#
+# Automatic building of the Mac OS X application bundle using py2app
+#
+# requires all libraries to be correctly installed! See distrib/OSX/README for details
+#
 
-rm -rf dummy.app
-py2applet --semi-standalone --iconfile icons/m221e.icns dummy.py
 
-#--packages
+function error()
+{
+	echo "$0: $1" 1>&2
+	exit -1
+}
+
+
+repo=${1:?Please specify the full path to the base directory of the repository tree}
+
+cd $repo || error "Specified dir does no exist"
+
+export PYTHONPATH=$PWD/contrib/:$PWD/src/:$PYTHONPATH
+
+py2applet --iconfile=${repo}/distrib/OSX/icons/m221e.icns ./src/bin/wxmaster.py ./src/bin/master.py ./src/bin/presenter.py ./src/bin/provider.py ./src/bin/maintainer.py
+
+mkdir -p ${repo}/src/bin/wxmaster.app/distrib/images/ || error "could not mkdir"
+cp ${repo}/distrib/images/* ${repo}/src/bin/wxmaster.app/distrib/images/ || error "could not cp"
+
+mkdir -p ${repo}/src/bin/wxmaster.app/distrib/applescript/ || error "could not mkdir"
+cp ${repo}/src/angel_app/wx/platform/mac/applescript/* ${repo}/src/bin/wxmaster.app/distrib/applescript/ || error "could not cp"
+
+echo "You app should now be in ${repo}/src/bin/"
