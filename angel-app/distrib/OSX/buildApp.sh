2c24961f6f79aaf057b7462e0db8704365fb72b8
pkremer <pkremer@82c71e93-f314-0410-b1e0-bdb1255beee9>
2007-03-12 17:39:21 +0000

    make it possible to give the application a name (e.g. NOT wxmaster)
    git-svn-id: http://svn.gna.org/svn/angel-app/trunk@712 82c71e93-f314-0410-b1e0-bdb1255beee9

--- angel-app/distrib/OSX/buildApp.sh
+++ angel-app/distrib/OSX/buildApp.sh
@@ -14,17 +14,23 @@ function error()
 
 
 repo=${1:?Please specify the full path to the base directory of the repository tree}
-
 cd $repo || error "Specified dir does no exist"
 
+RENAMETO="Angel-App.py" # is not allowed to contain empty spaces for now. This will set the application name that appears in the dock and other places
+BASENAME=`basename $RENAMETO .py`
+
 export PYTHONPATH=$PWD/contrib/:$PWD/src/:$PYTHONPATH
+cp ./src/bin/wxmaster.py ./src/bin/$RENAMETO
 
-py2applet --iconfile=${repo}/distrib/OSX/icons/m221e.icns ./src/bin/wxmaster.py ./src/bin/master.py ./src/bin/presenter.py ./src/bin/provider.py ./src/bin/maintainer.py
+py2applet --iconfile=${repo}/distrib/OSX/icons/m221e.icns ./src/bin/$RENAMETO
+#py2applet --iconfile=${repo}/distrib/OSX/icons/m221e.icns ./src/bin/wxmaster.py ./src/bin/master.py ./src/bin/presenter.py ./src/bin/provider.py ./src/bin/maintainer.py
 
-mkdir -p ${repo}/src/bin/wxmaster.app/distrib/images/ || error "could not mkdir"
-cp ${repo}/distrib/images/* ${repo}/src/bin/wxmaster.app/distrib/images/ || error "could not cp"
+mkdir -p ${repo}/src/bin/$BASENAME.app/distrib/images/ || error "could not mkdir"
+cp ${repo}/distrib/images/* ${repo}/src/bin/$BASENAME.app/distrib/images/ || error "could not cp"
 
-mkdir -p ${repo}/src/bin/wxmaster.app/distrib/applescript/ || error "could not mkdir"
-cp ${repo}/src/angel_app/wx/platform/mac/applescript/* ${repo}/src/bin/wxmaster.app/distrib/applescript/ || error "could not cp"
+mkdir -p ${repo}/src/bin/$BASENAME.app/distrib/applescript/ || error "could not mkdir"
+cp ${repo}/src/angel_app/wx/platform/mac/applescript/* ${repo}/src/bin/$BASENAME.app/distrib/applescript/ || error "could not cp"
 
-echo "You app should now be in ${repo}/src/bin/"
+rm ./src/bin/$RENAMETO
+
+echo "You app should now be in ${repo}/src/bin/$BASENAME.app"
