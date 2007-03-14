c307d54d677b3b33331190bdca352cf6ab7fc274
pkremer <pkremer@82c71e93-f314-0410-b1e0-bdb1255beee9>
2007-03-14 16:21:13 +0000

    add some high level key management methods
    git-svn-id: http://svn.gna.org/svn/angel-app/trunk@737 82c71e93-f314-0410-b1e0-bdb1255beee9

--- angel-app/src/angel_app/admin/secretKey.py
+++ angel-app/src/angel_app/admin/secretKey.py
@@ -10,7 +10,18 @@ AngelConfig = config.getConfig()
 from angel_app.log import getLogger
 log = getLogger(__name__)
 
-def createKey(filePath = os.path.join(AngelConfig.get("common","keyring"), "default.key")):
+def getKeyringDirectory():
+    """
+    Returns the fully qualified configured directory name. Makes sure
+    that it exists
+    """
+    directoryname = AngelConfig.get("common","keyring")
+    if os.path.exists(directoryname):
+        return directoryname
+    else:
+        raise NameError, "The keyring directory '%s' does not exist" % directoryname
+
+def createKey(filePath = os.path.join(getKeyringDirectory(), "default.key")):
     kk = ezKey()
     # TODO: make key size configurable
     kk.makeNewKeys() 
@@ -20,7 +31,7 @@ def createKey(filePath = os.path.join(AngelConfig.get("common","keyring"), "defa
 def createAtLeastOneKey():
     
     # where the keys are located
-    keyDirectory = AngelConfig.get("common", "keyring")
+    keyDirectory = getKeyringDirectory()
     
     # the keys that already exist
     keyFiles = os.listdir(keyDirectory)
@@ -31,5 +42,62 @@ def createAtLeastOneKey():
     if keyFiles == []:
         createKey()        
         # make sure the new key is globally visible
-        from angel_app.resource.local.internal import resource
-        resource.reloadKeys()
+        reloadKeyRing()
+
+def reloadKeyRing():
+    # make sure the new key is globally visible
+    # TODO: review: should/must this be in resource.local.internal?
+    from angel_app.resource.local.internal import resource
+    resource.reloadKeys()
+
+def isValidKey(streamObj):
+    """
+    checks if the given stream contains a valid key block
+    """
+    streamObj.seek(0)
+    kk = ezKey()
+    return kk.importKey(streamObj.read())
+
+def isValidKeyPair(streamObj):
+    """
+    check sif the given stream contains a key pair (public AND private key)
+    """
+    kk = ezKey()
+    streamObj.seek(0)
+    result = False
+    if isValidKey(streamObj):
+        log.debug("Valid key!")
+        streamObj.seek(0)
+        if kk.importKey(streamObj.read()):
+            log.debug("could import")
+            if len(kk.exportKeyPrivate()) > 0:
+                log.debug("could export")
+                result = True
+#    streamObj.seek(0)
+    return result
+
+def importKey(streamObj, keyname = "importedkey.key"):
+    """
+    will try to import the key pair from the given stream object,
+    """
+    import re
+    import angel_app.singlefiletransaction
+    fileextension = ".key"
+    # strip off a possibly existing .key extension:
+    m = re.compile("\.key$")
+    keyname = m.sub('', keyname)
+    log.debug("Trying to import a new key")
+    if isValidKeyPair(streamObj):
+        keyDirectory = getKeyringDirectory()
+        newkeyfilename = os.path.join(keyDirectory, keyname + fileextension)
+        if os.path.exists(newkeyfilename):
+            raise NameError, "A key with the name '%s' already exists" % keyname
+        t = angel_app.singlefiletransaction.SingleFileTransaction()
+        newkeyfile = t.open(newkeyfilename, 'wb')
+        streamObj.seek(0)
+        newkeyfile.write(streamObj.read())
+        t.commit()
+        reloadKeyRing()
+    else:
+        raise NameError, "This does not seem to be a valid key-pair!"
+    return True
