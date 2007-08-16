1a78ee8cd566a2fda7a6812299b09c395d2deafa
vincentk <vincentk@82c71e93-f314-0410-b1e0-bdb1255beee9>
2007-08-16 20:28:11 +0000

    adding docstrings
    git-svn-id: http://svn.gna.org/svn/angel-app/trunk@847 82c71e93-f314-0410-b1e0-bdb1255beee9

--- angel-app/src/angel_app/resource/__init__.py
+++ angel-app/src/angel_app/resource/__init__.py
@@ -1,1 +1,14 @@
+"""
+The angel-app defines three types of "views" on resources.
+
+-- remote resources are resources that are physically stored on a remote file 
+   system (and accessible via HTTP only).
+-- local resources are resources that are physically stored on a local file system.
+   they may be accessed via
+   * a "public" webdav interface, facing the internet (see local.external)
+   * a "secret" webdav interface, accessible only from the local host (see local.internal)
+   obviously, the security settings on the internal interface are considerably more
+   liberal than on the external one.
+
+"""
 all = ["basic", "local", "remote", "util"]
