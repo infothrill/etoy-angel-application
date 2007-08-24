0c1602e720b8e9798b3f250a6bb8be21ddfa42aa
vincentk <vincentk@82c71e93-f314-0410-b1e0-bdb1255beee9>
2007-08-24 13:28:28 +0000

    come to think of it, we don't provide PROPPATCH on the external interface either. since that's the only remaining thing that distinguishes resource.local.external from resource.local.Basic, we can discard all of resource.local.external, and just use Basic. move tests to resource.local.test, change provider.py to use Basic, and delete.
    git-svn-id: http://svn.gna.org/svn/angel-app/trunk@874 82c71e93-f314-0410-b1e0-bdb1255beee9

--- angel-app/src/angel_app/resource/local/external/methods/__init__.py
+++
@@ -1,6 +0,0 @@
-all = ["proppatch"]
-
-"""
-Mixins for AngelFile behaviour. Each mixin implements one WebDAV method (PUT, LOCK, etc.).
-I feel this is cleaner than wilfredo's bindmethods() - approach.
-"""
--- angel-app/src/angel_app/resource/local/external/test/__init__.py
+++
@@ -1,2 +0,0 @@
-all = ["methodTest",
-       "resourceTest"]
