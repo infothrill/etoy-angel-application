2d1b19dbc24080114e365d4182d81ead1a9990a5
pkremer <pkremer@82c71e93-f314-0410-b1e0-bdb1255beee9>
2007-09-09 17:06:43 +0000

    use mounttab from config file/default configuration
    git-svn-id: http://svn.gna.org/svn/angel-app/trunk@920 82c71e93-f314-0410-b1e0-bdb1255beee9

--- angel-app/src/angel_app/admin/initializeRepository.py
+++ angel-app/src/angel_app/admin/initializeRepository.py
@@ -1,9 +1,17 @@
 from angel_app.log import getLogger
+from angel_app.config.config import getConfig
 log = getLogger(__name__)
 
-fstab = [
-         ["http://missioneternity.org:6221", "MISSION ETERNITY"]
-         ]
+def getMountTab():
+    angelConfig = getConfig()
+    mounttab = []
+    if angelConfig.has_section('mounttab'):
+        for device in angelConfig.get('mounttab').keys():
+            mounttab.append( (device, angelConfig.get('mounttab', device) ) )
+
+    if len(mounttab) < 1:
+        log.debug("No mounts configured in mounttab in configuration")
+    return mounttab
 
 def initializeRepository():
 
@@ -28,7 +36,8 @@ def initializeRepository():
     from angel_app.admin.resourceProperties import setMountPoint
     from twisted.python.filepath import FilePath
     from angel_app.admin.resourceProperties import absPath
-
+    
+    fstab = getMountTab()
     for mount in fstab:
         log.info("mounting '%s' to '%s'" % (mount[0], mount[1]))
         if not FilePath(absPath(mount[1])).exists():
