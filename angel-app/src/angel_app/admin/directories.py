f9b564204196ff671637e472ff50e7ec70ea1c5f
pkremer <pkremer@82c71e93-f314-0410-b1e0-bdb1255beee9>
2007-10-12 15:45:56 +0000

    added routine to remove core angel-app directories in admin scenarios
    git-svn-id: http://svn.gna.org/svn/angel-app/trunk@928 82c71e93-f314-0410-b1e0-bdb1255beee9

--- angel-app/src/angel_app/admin/directories.py
+++ angel-app/src/angel_app/admin/directories.py
@@ -3,6 +3,8 @@ Utilities for creating the default repository directory layout.
 """
 
 import os
+import shutil
+
 from angel_app.config import config
 
 AngelConfig = config.getConfig()
@@ -11,7 +13,7 @@ def conditionalCreate(path):
     if not os.path.exists(path):
         os.mkdir(path)
     elif not os.path.isdir(path):
-        raise "Filesystem entry '%s' occupied, cannot create directory here." % path
+        raise Exception, "Filesystem entry '%s' occupied, cannot create directory here." % path
         
 def makeDirectories():
     conditionalCreate(AngelConfig.get("common", "angelhome"))
@@ -19,3 +21,15 @@ def makeDirectories():
     conditionalCreate(AngelConfig.get("common", "keyring"))
     conditionalCreate(AngelConfig.get("common", "logdir"))
     conditionalCreate(os.path.join(AngelConfig.get("common","angelhome"), "tmp"))
+    
+def removeDirectory(name):
+    """
+    High level method for removing core angel-app directories
+    
+    @param name: string specyfiyng the directory to remove 
+    """
+    dirs = {
+            'repository' : AngelConfig.get("common", "repository")
+            }
+    shutil.rmtree(dirs[name], ignore_errors = False)
+
