0b3b90fa6906df443986cf5beee848bc8dd587c7
pkremer <pkremer@82c71e93-f314-0410-b1e0-bdb1255beee9>
2007-11-10 11:08:39 +0000

    removed redundant information from docstrings (legalmatters)
    git-svn-id: http://svn.gna.org/svn/angel-app/trunk@1061 82c71e93-f314-0410-b1e0-bdb1255beee9

--- angel-app/src/angel_app/log.py
+++ angel-app/src/angel_app/log.py
@@ -2,47 +2,21 @@
 Logger for angel-app
 """
 
-legalMatters = """
- Copyright (c) 2006, etoy.VENTURE ASSOCIATION
- All rights reserved.
- 
- Redistribution and use in source and binary forms, with or without modification, 
- are permitted provided that the following conditions are met:
- *  Redistributions of source code must retain the above copyright notice, 
-    this list of conditions and the following disclaimer.
- *  Redistributions in binary form must reproduce the above copyright notice, 
-    this list of conditions and the following disclaimer in the documentation 
-    and/or other materials provided with the distribution.
- *  Neither the name of etoy.CORPORATION nor the names of its contributors may be used to 
-    endorse or promote products derived from this software without specific prior 
-    written permission.
- 
- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY 
- EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES 
- OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT 
- SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
- SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT 
- OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 
- HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, 
- OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
- SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
-"""
-
-author = """Paul Kremer, 2007"""
+legalMatters = """See LICENSE for details."""
+__author__ = """Paul Kremer, 2007"""
 
 """
 This is a module that provides simple wrapping services for logging.
 It is responsible for tying together possibbly different logging engines,
-configuring them ...<br/>
-Logfiles are in $HOME/.angel_app/log/<br/>
+configuring them ...
+Logfiles are in $HOME/.angel_app/log/
 The directory $HOME/.angel_app/log/ will automatically get created if needed.
-<p>
+
 See the documentation of the  
 <a href="http://docs.python.org/lib/module-logging.html">python logging module</a>
 and  
 <a href="http://twistedmatrix.com/projects/core/documentation/howto/logging.html">twisted.log</a>
 for more information.
-</p>
 """
 
 import logging
