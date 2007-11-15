0e22a3bcc93cf3d55f3f87fda90c85f58d987644
vincentk <vincentk@82c71e93-f314-0410-b1e0-bdb1255beee9>
2007-11-15 19:44:04 +0000

    clean up of test setUP()
    git-svn-id: http://svn.gna.org/svn/angel-app/trunk@1089 82c71e93-f314-0410-b1e0-bdb1255beee9

--- angel-app/src/angel_app/resource/local/test/localResourceTest.py
+++ angel-app/src/angel_app/resource/local/test/localResourceTest.py
@@ -52,19 +52,22 @@ class BasicResourceTest(resourceTest.ResourceTest):
     testDirPath = os.path.sep.join([repositoryPath, "TEST"])
     testFilePath = os.path.sep.join([testDirPath, "file.txt"])
     testText = "lorem ipsum"
+    
+    def makeTestDirectory(self):
+        os.mkdir(self.testDirPath)
+        self.testDirectory = Crypto(self.testDirPath) 
+        self.testDirectory._registerWithParent()
+        self.testDirectory._updateMetadata()
+        
+    def makeTestFile(self):
+        open(self.testFilePath, 'w').write(self.testText)
+        self.testFile = Crypto(self.testFilePath)
+        self.testFile._registerWithParent()
+        self.testFile._updateMetadata()    
 
     def setUp(self):
-        try:
-            os.mkdir(self.testDirPath)
-        except OSError, e:
-            print `e`
-
-        cc = Crypto(self.testDirPath) 
-        cc._registerWithParent()
-        cc._updateMetadata()
-        self.testResource = Basic(self.testDirPath)
-        open(self.testFilePath, 'w').write(self.testText)
-        
+        self.makeTestDirectory()
+        self.makeTestFile()
         
     def tearDown(self):
         Crypto(self.testDirPath).remove() 
@@ -73,32 +76,32 @@ class BasicResourceTest(resourceTest.ResourceTest):
         """
         @return: a C{True} if this resource is accessible, C{False} otherwise.
         """
-        assert self.testResource.exists()
+        assert self.testDirectory.exists()
 
     def testIsWritable(self):
-        assert self.testResource.isWritableFile()
+        assert self.testDirectory.isWritableFile()
         assert Basic(repositoryPath).isWritableFile()
 
     def testReadFile(self):
         assert self.testText == Basic(self.testFilePath).open().read()  
         
     def testSetProperty(self):
-        dp = self.testResource.getPropertyManager()
+        dp = self.testDirectory.getPropertyManager()
         
         testText = "foo"
         ee = elements.ResourceID.fromString(testText)
         dp.set(ee)
-        assert self.testResource.resourceID() == testText
+        assert self.testDirectory.resourceID() == testText
     
     def testLocation(self):
         """
         @return the resource's path relative to the site root.
         """
-        assert self.testResource.relativePath() == "/TEST/"
+        assert self.testDirectory.relativePath() == "/TEST/"
         
     def testOpen(self):
         from angel_app.resource.abstractContentManager import REPR_DIRECTORY
-        assert REPR_DIRECTORY == self.testResource.open().read()
+        assert REPR_DIRECTORY == self.testDirectory.open().read()
     
     def testIsCollection(self):
         """
@@ -106,19 +109,19 @@ class BasicResourceTest(resourceTest.ResourceTest):
         @return: a C{True} if this resource is a collection resource, C{False}
             otherwise.
         """
-        assert self.testResource.isCollection()
+        assert self.testDirectory.isCollection()
 
     def testResourceID(self):
         """
         @return: the id of the resource as C{String}.
         """
-        assert type(self.testResource.resourceID().toxml()) == type("")
+        assert type(self.testDirectory.resourceID().toxml()) == type("")
         
     def testRevision(self):
         """
         @return: a C{int} corresponding to the revision number of this resource
         """
-        revisionNumber = self.testResource.revision()
+        revisionNumber = self.testDirectory.revision()
         assert type(revisionNumber) == type(0)
         assert revisionNumber >= 0
 
@@ -127,42 +130,42 @@ class BasicResourceTest(resourceTest.ResourceTest):
         Make sure the stored public key is a valid ezPyCrypto key.
         """
         from angel_app.contrib.ezPyCrypto import key
-        publicKeyString = self.testResource.publicKeyString()
+        publicKeyString = self.testDirectory.publicKeyString()
         k = key()
         k.importKey(publicKeyString)
         
     def testPath(self):
         "The test resource is a directory, hence the relative URL"
         import urllib
-        url = self.testResource.relativeURL()
-        path = self.testResource.relativePath()
-        if self.testResource.isCollection():
+        url = self.testDirectory.relativeURL()
+        path = self.testDirectory.relativePath()
+        if self.testDirectory.isCollection():
             assert url[-1] == "/"
             assert path[-1] == os.sep
         assert urllib.url2pathname(url) == path
 
     def testIsRoot(self):
-        assert False == self.testResource.isRepositoryRoot()
+        assert False == self.testDirectory.isRepositoryRoot()
 
     def testFindChildren(self):
         """
         @return: an iterable over C{uri}.
         """
-        assert self.testResource.childLinks().qname() == Children.qname()
+        assert self.testDirectory.childLinks().qname() == Children.qname()
     
     def testStream(self):
         """
         @return: an object that minimally supports the read() method, which in turn returns the stream contents as a string.
         """
-        assert self.testResource.open().read() == REPR_DIRECTORY
+        assert self.testDirectory.open().read() == REPR_DIRECTORY
         
         
     def testClones(self):
         """
         Since the dirResource was freshly created, its clones must all be inherited from the parent.
         """
-        clones = self.testResource.clones()
-        parentClones = self.testResource.parent().clones()
+        clones = self.testDirectory.clones()
+        parentClones = self.testDirectory.parent().clones()
         assert len(clones) == len(parentClones)
         
     def testDefaultProperties(self):
@@ -171,7 +174,7 @@ class BasicResourceTest(resourceTest.ResourceTest):
         are of the same type as the element requested.
         """
         from angel_app.resource.local.propertyManager import defaultMetaData
-        dp = self.testResource.deadProperties()
+        dp = self.testDirectory.deadProperties()
         for element in defaultMetaData.keys():
             dme = defaultMetaData[element]
             assert element == dme(dp).qname()
@@ -182,14 +185,14 @@ class BasicResourceTest(resourceTest.ResourceTest):
         Set a property, read it back out and compare it with the original.
         """
         testProperty = rfc2518.Collection()
-        self.testResource.deadProperties().set(testProperty)
-        assert testProperty.qname() in self.testResource.deadProperties().list()
-        outProperty = self.testResource.deadProperties().get(testProperty.qname())
+        self.testDirectory.deadProperties().set(testProperty)
+        assert testProperty.qname() in self.testDirectory.deadProperties().list()
+        outProperty = self.testDirectory.deadProperties().get(testProperty.qname())
         assert testProperty.toxml() == outProperty.toxml()
         
     def testInterfaceCompliance(self):
         """
         Verify interface compliance.
         """
-        assert IAngelResource.implementedBy(self.testResource.__class__)
-        assert zope.interface.verify.verifyClass(IAngelResource, self.testResource.__class__) 
+        assert IAngelResource.implementedBy(self.testDirectory.__class__)
+        assert zope.interface.verify.verifyClass(IAngelResource, self.testDirectory.__class__) 
