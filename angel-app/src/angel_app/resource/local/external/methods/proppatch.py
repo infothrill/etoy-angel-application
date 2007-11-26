8427672ddc0f8b6b154c4dd7593833423530b668
vincentk <vincentk@82c71e93-f314-0410-b1e0-bdb1255beee9>
2007-11-26 10:56:31 +0000

    check if this clone is already registered _after_ doing the potential IP resolution (but check for None first)
    git-svn-id: http://svn.gna.org/svn/angel-app/trunk@1159 82c71e93-f314-0410-b1e0-bdb1255beee9

--- angel-app/src/angel_app/resource/local/external/methods/proppatch.py
+++ angel-app/src/angel_app/resource/local/external/methods/proppatch.py
@@ -252,17 +252,17 @@ def cloneHandler(property, store, request):
     newClone = newClones[0]
 
     newClone = pingBack(newClone, request)
-
+    
+    if not newClone:
+        error = "Can't connect to you. I will ignore you."
+        response = StatusResponse(responsecode.BAD_REQUEST, error)
+        return Failure(exc_value=HTTPError(response))
+    
     # check if this clone is already registered _after_ doing the potential
     # IP resolution    
     if newClone in residentClones:
         log.info("clone %s already registered." % `newClone`)
         # nothing needs to be done, pretend everything is fine
         return responsecode.OK
-    
-    if not newClone:
-        error = "Can't connect to you. I will ignore you."
-        response = StatusResponse(responsecode.BAD_REQUEST, error)
-        return Failure(exc_value=HTTPError(response))
             
     return defaultHandler(clonesToElement(residentClones + [newClone]), store)     
