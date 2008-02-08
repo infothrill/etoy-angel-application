64733dc97305469ffe570dfe50a8efdc66af7a55
pkremer <pkremer@82c71e93-f314-0410-b1e0-bdb1255beee9>
2008-02-08 15:55:12 +0000

    made this thing work with vanilla OS X (don't rely on fink) don't need env var TMPDIR annymore
    git-svn-id: http://svn.gna.org/svn/angel-app/trunk@1200 82c71e93-f314-0410-b1e0-bdb1255beee9

--- angel-app/distrib/preprelease.sh
+++ angel-app/distrib/preprelease.sh
@@ -14,29 +14,33 @@
 # Author: Paul Kremer <pol@etoy.com>
 #
 
+function end()
+{
+	exitcode=$1
+	rm -rf $THISTMPDIR > /dev/null 2>&1 # cleanup temp junk
+	exit $exitcode
+}
+
 function error()
 {
 	echo "$0: $1" 1>&2
-	rm -rf $THISTMPDIR > /dev/null 2>&1 # cleanup temp junk
-	exit -1
+	end -1
 }
 
 function usage()
 {
-	echo "Usage: `basename $0` [local|trunk|release] directory|tag VERSION"
+	echo "Usage: `basename $0` DESTDIR [local|trunk|release] directory|tag VERSION"
 	echo ""
 	echo "  Example usage:"
-	echo "     `basename $0` local ~/src/angel-app/  creates a prepared tree of a local working copy"
-	echo "     `basename $0` trunk                   creates a prepared tree of the current trunk"
-	echo "     `basename $0` release testtag  0.1.2  creates a prepared tree of tag 'testtag' and sets version to 0.1.2"
+	echo "     `basename $0` ~/build/ local ~/src/angel-app/  creates a prepared tree of a local working copy"
+	echo "     `basename $0` ~/build/ trunk                   creates a prepared tree of the current trunk"
+	echo "     `basename $0` ~/build/ release testtag  0.1.2  creates a prepared tree of tag 'testtag' and sets version to 0.1.2"
 	echo ""
-	exit
+	end 0
 }
 
 # setup a temp dir for temp junk:
-THISTMPDIR=${TMPDIR:?env variable TMPDIR must be set}/`basename $0`_$$
-mkdir $THISTMPDIR
-chmod 700 $THISTMPDIR # just be sure it's only readable by the current user!
+THISTMPDIR=$(mktemp -d -t `basename $0`)
 
 #clean env:
 env -i
@@ -45,7 +49,16 @@ if [ -z "$1" ]
 then
 	usage
 fi
-
+DESTDIR=$1
+if [ ! -d $DESTDIR ]
+then
+	error "Destination dir $DESTDIR does not exist"
+fi
+shift
+if [ -z "$1" ]
+then
+	usage
+fi
 MODE=$1
 
 SHORTNAME="angel_app"
@@ -59,32 +72,32 @@ then
 	RELEASE=$1
 	if [ -z $TAG ];
 	then
-		echo "Please specify a tag!" 1>&2
-		exit 1
+		error "Please specify a tag!"
 	fi
 	if [ -z $RELEASE ];
 	then
-		echo "Please specify a release number!" 1>&2
-		exit 1
+		error "Please specify a release number!"
 	fi
 	echo "release: tag (ABC_x_xx) '$TAG' and release (x.xx) '$RELEASE'"
-fi
-
-if [ "${MODE}" = "local" ]
+elif [ "${MODE}" = "local" ]
 then
 	shift
 	LOCALDIR=$1
 	echo "release: local from dir '$LOCALDIR'"
 	if [ -z $LOCALDIR ];
 	then
-		echo "Please specify a local source directory!" 1>&2
-		exit 1
+		error "Please specify a local source directory!"
 	fi
 	if [ ! -d $LOCALDIR ];
 	then
-		echo "Directory '$LOCALDIR' does not exist!" 1>&2
-		exit 1
+		error "Directory '$LOCALDIR' does not exist!"
 	fi
+elif [ "${MODE}" = "trunk" ]
+then
+	# NOP
+	echo "Trunk"
+else
+	error "unknown mode $MODE"
 fi
 
 cd ${THISTMPDIR} || error "Could not switch to directory $THISTMPDIR"
@@ -93,24 +106,14 @@ cd ${THISTMPDIR} || error "Could not switch to directory $THISTMPDIR"
 if [ "${MODE}" = "release" ]
 then
 	echo "Checking out fresh copy from SVN with tag ${TAG}...";
-	nice svn co ${SVNURL}/tags/${TAG} > /dev/null
-	if [ $? != 0 ];
-	then
-		echo "An error occured while checking out from repository!" 1>&2
-		exit 1
-	fi
+	svn co ${SVNURL}/tags/${TAG} > /dev/null || error "An error occured while checking out from repository!"
 	mv ${TAG} ${SHORTNAME}
 fi
 if [ "${MODE}" = "trunk" ]
 then
 	RELEASE="SVN"
 	echo "Checking out fresh copy from SVN...";
-	svn co ${SVNURL}/trunk/angel-app > /dev/null
-	if [ $? != 0 ];
-	then
-		echo "An error occured while checking out from repository!" 1>&2
-		exit 1
-	fi
+	svn co ${SVNURL}/trunk/angel-app > /dev/null || error "An error occured while checking out from repository!"
 	mv angel-app ${SHORTNAME}
 fi
 if [ "${MODE}" = "local" ]
@@ -118,7 +121,7 @@ then
 	RELEASE="LOCALDEV"
 	echo "Clean local copy..."
 	rm -rf ${THISTMPDIR}/${SHORTNAME}
-	cp -ax $LOCALDIR ${SHORTNAME}  > /dev/null
+	cp -R $LOCALDIR ${SHORTNAME}  > /dev/null || error "Error while copying local data"
 fi
 
 # get the revision number from svn:
@@ -142,7 +145,7 @@ echo ""
 twistedtar=`ls ${SHORTNAME}/twisted_trunk_*.tar.bz2`
 twistedsrc=`basename $twistedtar .tar.bz2`
 echo "Unpacking twisted from ${twistedtar} to ${SHORTNAME}/src/"
-tar xjf ${twistedtar}
+tar xjf ${twistedtar} || error "Could not untar ${twistedtar}"
 rm -rf ${SHORTNAME}/src/twisted/
 mv ${twistedsrc}/twisted/ ${SHORTNAME}/src/
 rm -rf ${twistedsrc}
@@ -165,11 +168,11 @@ fi
 rm -rf "${SHORTNAME}-${RELEASE}"
 #rm -rf "${SHORTNAME}-${RELEASE}.tgz"
 
-mv ${SHORTNAME} "${SHORTNAME}-${RELEASE}"
-rm -rf "$TMPDIR/${SHORTNAME}-${RELEASE}" > /dev/null 2>&1
-mv "${SHORTNAME}-${RELEASE}" $TMPDIR
+mv ${SHORTNAME} "${SHORTNAME}-${RELEASE}" || error "Could not rename to shortname-release"
+rm -rf "$DESTDIR/${SHORTNAME}-${RELEASE}" || error "Could not remove data from DESTDIR"
+mv "${SHORTNAME}-${RELEASE}" $DESTDIR || error "Could not move to DESTDIR"
 # clean temp junk:
-rm -rf $THISTMPDIR > /dev/null 2>&1 # cleanup temp junk
 echo "BUILD_ID: ${BUILD_ID}"
 echo "You can now create a platform build from the directory:"
-echo "${TMPDIR}${SHORTNAME}-${RELEASE}"
+echo "${DESTDIR}${SHORTNAME}-${RELEASE}"
+end 0
