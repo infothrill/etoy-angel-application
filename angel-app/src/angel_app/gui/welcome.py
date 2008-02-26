fc8942b890c627556d32e99863fa2224c37380f9
pkremer <pkremer@82c71e93-f314-0410-b1e0-bdb1255beee9>
2008-02-26 23:12:15 +0000

    nicely center welcome screen picture, assertion check on image file
    git-svn-id: http://svn.gna.org/svn/angel-app/trunk@1201 82c71e93-f314-0410-b1e0-bdb1255beee9

--- angel-app/src/angel_app/gui/welcome.py
+++ angel-app/src/angel_app/gui/welcome.py
@@ -6,14 +6,17 @@ from angel_app.gui.compat import wrap as platformwrap
 class WelcomePanel(wx.Panel):
     def __init__(self, parent, statuslog):
         wx.Panel.__init__(self, parent, -1)
-        Sizer = wx.BoxSizer(wx.HORIZONTAL)
+        Sizer = wx.BoxSizer(wx.VERTICAL)
 
-        bmp = wx.Bitmap(os.path.join(platformwrap.getResourcePath(), "images", "angel_app_welcomescreen.jpg"), wx.BITMAP_TYPE_JPEG)
-        pic = wx.StaticBitmap(self, -1, bmp, wx.Point(-1, -1))
-        self.SetForegroundColour(wx.WHITE)
         self.SetBackgroundColour(wx.WHITE)
+        bitmapfilename = os.path.join(platformwrap.getResourcePath(), "images", "angel_app_welcomescreen.jpg")
+        assert os.path.isfile(bitmapfilename), "Welcome screen picture '%s' could not be found" % bitmapfilename
+        bmp = wx.Bitmap(bitmapfilename, wx.BITMAP_TYPE_JPEG)
+        pic = wx.StaticBitmap(self, -1, bmp, wx.Point(bmp.GetWidth(), bmp.GetHeight()))
 
-        Sizer.Add(pic, 0, wx.ALL | wx.CENTRE, 0)
-
+        # adding stretchable space before and after centers the image.
+        Sizer.Add((1,1),1)
+        Sizer.Add(pic, 0, wx.ALL | wx.ALIGN_CENTER_HORIZONTAL | wx.ADJUST_MINSIZE, 0)
+        Sizer.Add((1,1),1)
+   
         self.SetSizer(Sizer)
-
