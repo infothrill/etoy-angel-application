4aad5cf229c924bb2edbb646cc564099fc376b26
pkremer <pkremer@82c71e93-f314-0410-b1e0-bdb1255beee9>
2008-02-27 10:05:47 +0000

    tempdir fix, use py2app for copying support files to the application bundle
    git-svn-id: http://svn.gna.org/svn/angel-app/trunk@1205 82c71e93-f314-0410-b1e0-bdb1255beee9

--- angel-app/distrib/OSX/buildApp.sh
+++ angel-app/distrib/OSX/buildApp.sh
@@ -26,9 +26,7 @@ function error()
 }
 
 # setup a temp dir for temp junk:
-THISTMPDIR=${TMPDIR:?env variable TMPDIR must be set}/`basename $0`_$$
-mkdir $THISTMPDIR
-chmod 700 $THISTMPDIR # just be sure it's only readable by the current user!
+THISTMPDIR=$(mktemp -d -t `basename $0`XXXXXX)
 
 if [ -z $1 ]
 then
@@ -75,11 +73,13 @@ echo "Pythonpath: " $PYTHONPATH
 
 echo "Running py2applet..."
 rm -rf "${SCRIPTDIR}/${APPNAME}.app" # make sure it's not there yet
-py2applet --iconfile=${repo}/distrib/OSX/icons/m221e.icns "${SCRIPTDIR}/Angel.py" "${SCRIPTDIR}/master.py" "${SCRIPTDIR}/presenter.py" "${SCRIPTDIR}/provider.py" "${SCRIPTDIR}/maintainer.py" > $THISTMPDIR/py2applet.log 2>&1
+py2app_opts="--iconfile=${repo}/distrib/OSX/icons/m221e.icns --resources=${repo}/distrib/applescript,${repo}/distrib/images,${repo}/distrib/files"
+#python setup.py py2app --use-pythonpath $py2app_opts
+py2applet $py2app_opts "${SCRIPTDIR}/Angel.py" "${SCRIPTDIR}/master.py" "${SCRIPTDIR}/presenter.py" "${SCRIPTDIR}/provider.py" "${SCRIPTDIR}/maintainer.py" > $THISTMPDIR/py2applet.log 2>&1
 if [[ $? -ne 0 ]]
 then
-	mv $THISTMPDIR/py2applet.log ${TMPDIR}/py2applet.log
-	error "py2applet exited with a non-zero exit code. Check ${TMPDIR}/py2applet.log for details"
+#	cat ${THISTMPDIR}/py2applet.log
+	error "py2applet exited with a non-zero exit code"
 fi
 
 echo "Moving Application bundle..."
@@ -87,14 +87,13 @@ rm -rf "${repo}/${APPNAME}.app" # make sure it's not there yet
 mv "${SCRIPTDIR}/${APPNAME}.app" "${repo}"
 BUNDLE="${repo}/${APPNAME}.app"
 
-echo "Adding resources to application Bundle..."
-mkdir -p "${BUNDLE}/Resources/images/" || error "could not mkdir"
-cp ${repo}/distrib/images/* "${BUNDLE}/Resources/images/" || error "could not cp"
-mkdir -p "${BUNDLE}/Resources/files/" || error "could not mkdir"
-cp ${repo}/distrib/files/* "${BUNDLE}/Resources/files/" || error "could not cp"
-
-mkdir -p "${BUNDLE}/Resources/applescript/" || error "could not mkdir"
-cp ${repo}/distrib/applescript/* "${BUNDLE}/Resources/applescript/" || error "could not cp"
+#echo "Adding resources to application Bundle..."
+#mkdir -p "${BUNDLE}/Resources/images/" || error "could not mkdir"
+#cp ${repo}/distrib/images/* "${BUNDLE}/Resources/images/" || error "could not cp"
+#mkdir -p "${BUNDLE}/Resources/files/" || error "could not mkdir"
+#cp ${repo}/distrib/files/* "${BUNDLE}/Resources/files/" || error "could not cp"
+#mkdir -p "${BUNDLE}/Resources/applescript/" || error "could not mkdir"
+#cp ${repo}/distrib/applescript/* "${BUNDLE}/Resources/applescript/" || error "could not cp"
 
 echo "Patching Info.plist to match our info..."
 "${repo}/distrib/OSX/patchPlist.py" --version "$VERSION" --buildnumber "$BUILD_ID" "${BUNDLE}"
