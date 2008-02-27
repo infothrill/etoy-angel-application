152e3ac867ff22c4268628e5c1d3f532c6f28801
pkremer <pkremer@82c71e93-f314-0410-b1e0-bdb1255beee9>
2008-02-27 00:12:56 +0000

    minimal: code style only
    git-svn-id: http://svn.gna.org/svn/angel-app/trunk@1202 82c71e93-f314-0410-b1e0-bdb1255beee9

--- angel-app/src/bin/provider.py
+++ angel-app/src/bin/provider.py
@@ -1,16 +1,17 @@
 """
-DAV server that runs on the 'external' interface -- i.e. the 
-communicates with other angel-app instances.
+Essentially the DAV server that runs on the 'external' interface -- i.e. the 
+server that provides local data to other ANGEL APPLICATION instances.
+
 This guy is NOT safe and MUST NOT gain access to the secret key(s). 
-I.e. it may NOT commit data to the angel-app (except e.g. new clone
+I.e. it may NOT commit data to the local repository (except e.g. new clone
 metadata).
 """
 
 def main():
-    import angel_app.proc.provider
+    import angel_app.proc.provider as provider
 
-    options = angel_app.proc.provider.boot()
-    angel_app.proc.provider.dance(options)
+    options = provider.boot()
+    provider.dance(options)
 
 
 if __name__ == "__main__":
