b35fad2083db3e5c3821fe3bab6f20053a3a4bf7
vincentk <vincentk@82c71e93-f314-0410-b1e0-bdb1255beee9>
2008-03-02 16:42:57 +0000

    zeo not crashing
    git-svn-id: http://svn.gna.org/svn/angel-app/trunk@1235 82c71e93-f314-0410-b1e0-bdb1255beee9

--- angel-app/src/angel_app/proc/zeo.py
+++ angel-app/src/angel_app/proc/zeo.py
@@ -346,11 +346,43 @@ def init_signames():
 
 # Main program
 
+def getFileStorage():
+    from ZODB.config import FileStorage # That's a FileStorage *opener*!
+    class FSConfig:
+        def __init__(self, name, path):
+            self._name = name
+            self.path = path
+            self.create = 0
+            self.read_only = 0
+            self.stop = None
+            self.quota = None
+        def getSectionName(self):
+            return self._name
+    #name = str(1 + len(self.storages))
+    # From StorageServer constructor docs:
+    # By convention, storage names are typically
+    # strings representing small integers starting at '1'.
+    UNIQUE_NAME_FOR_STORAGE = '1' 
+    opener = FileStorage(
+                       FSConfig(
+                                UNIQUE_NAME_FOR_STORAGE, 
+                                "/Users/vincent/foo.fs"))
+    return {UNIQUE_NAME_FOR_STORAGE : opener.open()}
+
+def getZEOServer(storage):
+    from ZEO.StorageServer import StorageServer
+    address = ("127.0.0.1", 6223)
+    return StorageServer(address, storage)
+
 def main(args=None):
-    options = ZEOOptions()
-    options.realize(args)
-    s = ZEOServer(options)
-    s.main()
+    getZEOServer(getFileStorage())
+
+    import ThreadedAsync.LoopCallback
+    ThreadedAsync.LoopCallback.loop()
+    #options = ZEOOptions()
+    #options.realize(args)
+    #s = ZEOServer(options)
+    #s.main()
 
 if __name__ == "__main__":
     main()
