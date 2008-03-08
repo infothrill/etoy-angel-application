b127a2aee1b717f6f921014b01edc9c62b763fea
pkremer <pkremer@82c71e93-f314-0410-b1e0-bdb1255beee9>
2008-03-08 13:06:16 +0000

    revamp. workarounds. ez not ez. works on unix. mac os x gui fails (10.5) because it doesn't set sys.executable to 'pythonw' although it should (when started with it of course). messy stuff.
    git-svn-id: http://svn.gna.org/svn/angel-app/trunk@1275 82c71e93-f314-0410-b1e0-bdb1255beee9

--- angel-app/bootstrap.py
+++ angel-app/bootstrap.py
@@ -1,44 +1,55 @@
 #!/usr/bin/env python
 
-INSTALL_LOCATION = "angel-app"
-
 import os
 import sys
+import shutil
+import subprocess
 
 def check_python():
     if sys.version_info < (2,4):
-        print "Python 2.4 or higher is required to run Angel-App."
+        print "Python 2.4 or higher is required to run Angel."
         sys.exit(256)
 
 check_python()
 
-os.mkdir(INSTALL_LOCATION)
+if len(sys.argv) < 2:
+    print "Please provide an install target directory as parameter (must not exist, will be created)"
+    sys.exit(1)
+else:
+    INSTALL_LOCATION = sys.argv[1]
+    assert not os.path.exists(INSTALL_LOCATION), "Install dir '%s' already exists. Aborting." % INSTALL_LOCATION 
+    os.mkdir(INSTALL_LOCATION)
 
-import commands
-def run(command, description):  
-  print description
-  restuple = commands.getstatusoutput(command)
-  if restuple[0] != 0:
-      print "Command %s returned non-zero exit code '%s' and output '%s'" % (command, restuple[0], restuple[1])
-      sys.exit(256)
-  else:
-      print command + ":" + restuple[1]
+def run(command, description):
+    print description
+    proc = subprocess.Popen(command, stdout=sys.stdout, stderr=sys.stderr, env=os.environ, stdin=None)
+    alive = True
+    while alive:
+        exitcode = proc.poll()
+        if exitcode is not None: alive = False
+    if not exitcode == 0:
+        raise Exception, "Error running command '%s' (exit code: %s)" %( " ".join(cmd), exitcode)
 
-run(
-  sys.executable + " ./virtual-python.py --prefix=" + INSTALL_LOCATION,
-  "create virtual python installation")
+cmd = [sys.executable, "./virtual-python.py", "--prefix", INSTALL_LOCATION]
+run(cmd, "create virtual python installation")
+executable = os.path.join(INSTALL_LOCATION, 'bin', 'python')
+assert os.path.isfile(executable)
+print "using virtual python installation from now on"
 
-print "use virtual python installation from now on"
-os.environ["PATH"] = INSTALL_LOCATION + "/bin"  + ":" + os.environ["PATH"] 
-print os.environ["PATH"]
+# According to http://peak.telecommunity.com/DevCenter/EasyInstall#creating-a-virtual-python, we must clear any previously set PYTHONPATH
+# but then it doesn't work. Also, to have it install the stuff into our virtual python's site-packages dir, we MUST set the PYTHONPATH:  
+py_version = 'python%s.%s' % (sys.version_info[0], sys.version_info[1])
+ourlibdir = INSTALL_LOCATION + '/lib/' +py_version+'/site-packages/'
+os.environ['PYTHONPATH'] = ourlibdir
+cmd = [executable, "./ez_setup.py"]
+run(cmd, "running ez_setup.py")
 
-run(
-  "python ./ez_setup.py",
-  "running ez_setup.py")
+cmd = [executable, "./setup.py",  "install", "--prefix", INSTALL_LOCATION]
+run(cmd, "install Angel libraries")
 
-run("python ./setup.py install",
- "install angel-app libraries")
-
-run("cp src/bin/* " + INSTALL_LOCATION + "/bin",
-  "installing angel-app binaries")
-
+print "install Angel binaries"
+for fname in os.listdir(os.path.join(os.getcwd() ,'src/bin/')):
+    src = os.path.join(os.getcwd() ,'src/bin/', fname)
+    dest = os.path.join(INSTALL_LOCATION, "bin", os.path.basename(fname))
+    if os.path.isfile(src):
+        shutil.copyfile(src, dest)
