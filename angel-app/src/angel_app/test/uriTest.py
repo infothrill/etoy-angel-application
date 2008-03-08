e8eca034ae1d87a8d25d21c1ed7a5a3c52316e67
pkremer <pkremer@82c71e93-f314-0410-b1e0-bdb1255beee9>
2008-03-08 14:13:24 +0000

    duplicate signature fix
    git-svn-id: http://svn.gna.org/svn/angel-app/trunk@1284 82c71e93-f314-0410-b1e0-bdb1255beee9

--- angel-app/src/angel_app/test/uriTest.py
+++ angel-app/src/angel_app/test/uriTest.py
@@ -11,13 +11,7 @@ class URITest(unittest.TestCase):
 
     def testBasicURI(self):
         _uri = "http://missioneternity.org:6221/"
-        pp = uri.parse(_uri)
-        #print "PARSE: ", pp.dump()
-        #print "parse host: ", pp.host
-    
-    def testBasicURI(self):
-        _uri = "http://missioneternity.org:6221/"
-        pp = uri.parse(_uri)
+        dummy = uri.parse(_uri)
         #print "PARSE: ", pp.dump()
         #print "parse host: ", pp.host
         
