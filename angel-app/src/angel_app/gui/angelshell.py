71e8198ae84dd9caba9fbed25d65bf958e609114
pkremer <pkremer@82c71e93-f314-0410-b1e0-bdb1255beee9>
2008-06-11 21:26:02 +0000

    added KeyPress events for Cmd-C, ESC and Cmd-W to the angelshell
    git-svn-id: http://svn.gna.org/svn/angel-app/trunk@1356 82c71e93-f314-0410-b1e0-bdb1255beee9

--- angel-app/src/angel_app/gui/angelshell.py
+++ angel-app/src/angel_app/gui/angelshell.py
@@ -1,9 +1,16 @@
+"""
+A module providing an interactive python shell in wx
+"""
 
 import wx
 import wx.py as py
 
-from angel_app.config.config import getConfig
+_ = wx.GetTranslation
 
+from angel_app.config.config import getConfig
+from angel_app.log import getLogger
+
+log = getLogger(__name__)
 
 # executed by angelshell if no angelshellinit.py is found:
 defaultShellCommands = [
@@ -23,10 +30,32 @@ class AngelShellWindow(wx.Frame):
         wx.Frame.__init__(self, parent, id, title, size=(500, 300))
         self.Statusbar = self.CreateStatusBar(1, 0)
         Sizer = wx.BoxSizer(wx.VERTICAL)
-        Sizer.Add(angelshell(parent = self), proportion = 2, flag=wx.ALL|wx.EXPAND, border = 1)
+        self.shellpanel = angelshell(parent = self) 
+        self.shellpanel.Bind(wx.EVT_KEY_DOWN, self.OnKeyDown)
+        Sizer.Add(self.shellpanel, proportion = 2, flag=wx.ALL|wx.EXPAND, border = 1)
         self.SetSizer(Sizer)
         self.CentreOnParent()
         self.Show(True)
+
+    def OnKeyDown(self, event):
+        """
+        catch and handle a couple of special keys like Cmd-C or Esc
+        
+        @param event: wx.KeyEvent
+        """
+        keycode = event.GetKeyCode()
+        log.debug("OnKeyDown() got keycode: %s" % keycode)
+        if keycode == wx.WXK_ESCAPE:
+            ret  = wx.MessageBox(_('Quit the shell?'), _('Question'), wx.YES_NO | wx.CENTRE | wx.NO_DEFAULT, self)
+            if ret == wx.YES:
+                self.Close()
+        elif event.CmdDown(): # Cmd or Ctrl
+            if keycode == 67: # 'c'
+                self.shellpanel.Copy()
+            if keycode == 87: # 'w'
+                self.Close()
+        event.Skip() # pass it up the hierarchy!
+
 
 def angelshell(parent):
     """
@@ -47,3 +76,13 @@ def angelshell(parent):
         win.Execute(command)
     return win
 
+if __name__ == '__main__':
+    """
+    This allows us to run it separately from the rest of the GUI
+    """
+    from angel_app.log import initializeLogging
+    initializeLogging()
+    app = wx.App(0)
+    app.config = getConfig()
+    AngelShellWindow(None, -1, _('Angelshell'))
+    app.MainLoop()
