945de9b246a4cf23bfc3e13189f10fcd5d25bd5d
vincentk <vincentk@82c71e93-f314-0410-b1e0-bdb1255beee9>
2008-06-14 11:21:32 +0000

    use the API
    git-svn-id: http://svn.gna.org/svn/angel-app/trunk@1364 82c71e93-f314-0410-b1e0-bdb1255beee9

--- angel-app/src/angel_app/config/internal.py
+++ angel-app/src/angel_app/config/internal.py
@@ -3,19 +3,19 @@ from os import sep, listdir
 
 from angel_app.config.config import getConfig
 from angel_app.log import getLogger
+from angel_app.admin import secretKey
+
 
 log = getLogger(__name__)
 angelConfig = getConfig()
 
-keyBase = angelConfig.get("common", "keyring")
-
 # TODO: it seems odd to have one module for a single function. review.
+# fix: should go into admin/secretKey
 
-def loadKeysFromFile(keyDir = keyBase):
+def loadKeysFromFile(keyDir = secretKey.getKeyringDirectory()):
     """
     Load the ***SECRET*** keys from the appropriate location in the angel-app directory.
-    """
-    
+    """ 
     keyFiles = listdir(keyDir)
     keyRing = {}
     for fileName in keyFiles:
