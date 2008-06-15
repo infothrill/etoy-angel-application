a28bd36628bb49c1d831392c24b5262dbe379c81
pkremer <pkremer@82c71e93-f314-0410-b1e0-bdb1255beee9>
2008-06-15 18:19:51 +0000

    no more zeo
    git-svn-id: http://svn.gna.org/svn/angel-app/trunk@1380 82c71e93-f314-0410-b1e0-bdb1255beee9

--- angel-app/src/scripts/createModuleDepGraph.sh
+++ angel-app/src/scripts/createModuleDepGraph.sh
@@ -15,7 +15,7 @@ then
 	exit -1
 fi
 
-for f in zeo master presenter provider maintainer angel
+for f in master presenter provider maintainer angel
 do
 	`PYTHONPATH=src/:$PYTHONPATH python src/scripts/py2depgraph.py src/bin/$f.py | python src/scripts/depgraph2dot.py | dot -T $format -o $outdir/$f.$format`
 done
