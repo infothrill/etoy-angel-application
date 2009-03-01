a0458c53428ad0cca624c752d1af26ce53074ff9
vincentk <vincentk@82c71e93-f314-0410-b1e0-bdb1255beee9>
2009-03-01 18:36:41 +0000

    adding quota support
    git-svn-id: http://svn.gna.org/svn/angel-app/trunk@1397 82c71e93-f314-0410-b1e0-bdb1255beee9

--- angel-app/src/angel_app/maintainer/quota.py
+++ angel-app/src/angel_app/maintainer/quota.py
@@ -11,33 +11,10 @@ import time
 log = getLogger(__name__)
 AngelConfig = config.getConfig()
 
-quotas = QuotaManager(
-                      AngelConfig.getint("common", "defaultquota"),
-                      Basic(AngelConfig.get("common","repository")),
-                      __readQuotasFromConfig(
-                                             AngelConfig.get("mounttab"),
-                                             AngelConfig.get("quotas"))
-                      )
-        
-def __readQuotasFromConfig(mounts, quotas):
-    mounts = [name for name in mounts.itervalues()]
-    
-    qq = {}
-    for mount in config.iterkeys():
-        if mount not in mounts:
-            log.warn("No mount point defined for quota specification: " + `mout`)
-        else:
-            myQ = config.getint(mount)
-            qq[Basic(mount).keyUUID()] = myQ
-            log.info("Added quota of "  + myQ + " for mount point: " + mount)
-            
-    return qq
-            
-        
         
     
 class QuotaManager:
-    def __init__(self, default_, root_, quotas_):
+    def __init__(self, default_, root_, quotas_ = None):
         self.default = default_
         self.root = root_
         self.quotas = quotas_
@@ -58,7 +35,7 @@ class QuotaManager:
             return 0
         
         # otherwise, it's a mounted resource
-        quota = self.quotas[resource.keyUUID()]
+        quota = None != self.quotas and self.quotas[resource.keyUUID()] or None
         
         if quota is None:
             # no default quota is defined, return default:
@@ -66,3 +43,51 @@ class QuotaManager:
         else:
             # quota defined:
             return quota
+        
+def __readQuotasFromConfig(mounts, quotas):
+    mounts = [name for name in mounts.itervalues()]
+    
+    qq = {}
+    
+    if quotas is None:
+        return qq
+    
+    for mount in quotas.iterkeys():
+        if mount not in mounts:
+            log.warn("No mount point defined for quota specification: " + `mout`)
+        else:
+            myQ = quotas.getint(mount)
+            qq[Basic(mount).keyUUID()] = myQ
+            log.info("Added quota of "  + myQ + " for mount point: " + mount)
+            
+    return qq
+
+def __createQuotas():
+    
+    # the default's default: if no default provided, fall back to
+    # 100 MB
+    oneMeg = 1024 ** 3
+    default = 100 * oneMeg
+    try:
+        default = AngelConfig.getint("common", "defaultquota") 
+    except:
+        log.warn("No default quota provided. Consider specifying common/defaultquota in the config.")
+    log.info("Default quota set to " + `default / oneMeg` + " MiB per mount.")
+    
+    quotas = None
+    try:
+        default = AngelConfig.get("quotas")
+    except:
+        log.warn("No per-mount point quotas specified. Consider specifying a 'quotas' secsion in the config.")
+    
+    
+    QuotaManager(
+                      default,
+                      Basic(AngelConfig.get("common","repository")),
+                      __readQuotasFromConfig(
+                                             AngelConfig.get("mounttab"),
+                                             quotas)
+                      )
+
+quotas = __createQuotas()
+    
