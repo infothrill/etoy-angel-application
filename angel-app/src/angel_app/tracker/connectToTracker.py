104a124b80a88392c28f5c72045850fd1053678f
pkremer <pkremer@82c71e93-f314-0410-b1e0-bdb1255beee9>
2009-05-22 13:39:20 +0000

    ups, wrong number of parameters. sorry.
    git-svn-id: http://svn.gna.org/svn/angel-app/trunk@1448 82c71e93-f314-0410-b1e0-bdb1255beee9

--- angel-app/src/angel_app/tracker/connectToTracker.py
+++ angel-app/src/angel_app/tracker/connectToTracker.py
@@ -4,13 +4,15 @@
 based on http://twistedmatrix.com/projects/web2/documentation/examples/intro/simple.py
 """
 
+from angel_app.log import getLogger
 from angel_app.resource.remote.httpRemote import HTTPRemote
-
 from angel_app.tracker.tracker import TRACKER_PORT
+
+log = getLogger(__name__)
 
 def connectToTracker():
     try:
-        tracker = HTTPRemote("missioneternity.org", TRACKER_PORT)
+        tracker = HTTPRemote("missioneternity.org", TRACKER_PORT, "/")
         statistics = tracker.performRequestWithTimeOut("GET").read()
         return statistics
     except:
@@ -18,8 +20,9 @@ def connectToTracker():
 
 def pingTracker():
     try:
-        tracker = HTTPRemote("missioneternity.org", TRACKER_PORT)
+        tracker = HTTPRemote("missioneternity.org", TRACKER_PORT, "/")
         dummyresponse = tracker.performRequestWithTimeOut("HEAD")
         return True
-    except:
+    except Exception, e:
+        log.debug("Tracker ping failed", exc_info = e)
         return False
