0a9c3778f3bdf027287051245f1db5d485c4c560
pkremer <pkremer@82c71e93-f314-0410-b1e0-bdb1255beee9>
2009-05-24 15:00:55 +0000

    removed garbage comments
    git-svn-id: http://svn.gna.org/svn/angel-app/trunk@1455 82c71e93-f314-0410-b1e0-bdb1255beee9

--- angel-app/src/angel_app/gui/compat/unix.py
+++ angel-app/src/angel_app/gui/compat/unix.py
@@ -1,10 +1,3 @@
-
-#for (int count = 0; count < browsers.length && browser == null; count++)
-#if (Runtime.getRuntime().exec(new String[] {"which", browsers[count]}).waitFor() == 0) browser = browsers[count]; 
-#if (browser == null) throw new Exception("Could not find web browser"); else Runtime.getRuntime().exec(new String[] {browser, url}); } }
-#catch (Exception e) { JOptionPane.showMessageDialog(null, errMsg + ":\n" + e.getLocalizedMessage()); } } }
-
-
 """
 Module for Unix/Linux specific methods
 """
