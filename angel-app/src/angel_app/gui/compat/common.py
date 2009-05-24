3863287fadffa2d8ef89348899a928da690aedba
pkremer <pkremer@82c71e93-f314-0410-b1e0-bdb1255beee9>
2009-05-24 14:52:07 +0000

    use os.environ['RESOURCEPATH']
    git-svn-id: http://svn.gna.org/svn/angel-app/trunk@1453 82c71e93-f314-0410-b1e0-bdb1255beee9

--- angel-app/src/angel_app/gui/compat/common.py
+++ angel-app/src/angel_app/gui/compat/common.py
@@ -4,8 +4,9 @@ def getResourcePath():
     """
     Returns the path containing the resources (images, icons, platform scripts)
     """
+    if 'RESOURCEPATH' in os.environ:
+        return os.environ['RESOURCEPATH'] # on mac os x, this is set for executables in an app bundle
     if os.path.exists('../../distrib/'): # sort of a hack when running from the command line (bin/)
         return '../../distrib/'
     else:
-        # with py2app, the cwd is Contents/Resources/ of the app-bundle
-        return os.getcwd()
+        return os.getcwd() # fallback with at least an existing path. it's a wild guess though.
