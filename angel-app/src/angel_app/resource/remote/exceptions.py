6d2a35671fc748540d9ba00c9102c8216344a6a0
pkremer <pkremer@82c71e93-f314-0410-b1e0-bdb1255beee9>
2009-08-18 17:13:00 +0000

    exceptions
    git-svn-id: http://svn.gna.org/svn/angel-app/trunk@1554 82c71e93-f314-0410-b1e0-bdb1255beee9

--- angel-app/src/angel_app/resource/remote/exceptions.py
+++ angel-app/src/angel_app/resource/remote/exceptions.py
@@ -1,9 +1,19 @@
-class CloneError(Exception):
+class BaseCloneError(Exception):
+    # don't use directly
     def __init__(self,value):
         self.parameter=value
     def __str__(self):
         return repr(self.parameter)
+    def __repr__(self):
+        return "%s(%s)" % (self.__class__.__name__, repr(self.parameter))
 
-class CloneNotFoundError(CloneError):
+class CloneError(BaseCloneError):
+    # logic error?
+    pass
+class CloneNotFoundError(BaseCloneError):
+    # http not found
+    pass
+class CloneIOError(BaseCloneError):
+    # socket et al
     pass
 
