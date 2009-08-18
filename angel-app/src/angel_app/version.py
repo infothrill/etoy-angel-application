b88a350e36d173afc195b53c6a22203d6902bdc2
pkremer <pkremer@82c71e93-f314-0410-b1e0-bdb1255beee9>
2009-08-18 11:29:04 +0000

    return a nicer looking fallback version string if nonne is set
    git-svn-id: http://svn.gna.org/svn/angel-app/trunk@1538 82c71e93-f314-0410-b1e0-bdb1255beee9

--- angel-app/src/angel_app/version.py
+++ angel-app/src/angel_app/version.py
@@ -6,8 +6,14 @@ version information.
 """
 
 def getVersionString():
+    """
+    Returns a string containing the version identifier. If no version is set,
+    this will return the string '221e'.
+    """
     # NOTE: VERSION and BUILD_ID number MUST be set during packaging! (most simply through sed/search+replace)
     v = "#!#VERSION#!#"
+    if v[0] == "#":
+        return "221e" # nicer looking fallback value
     return v
 
 def getBuildString():
